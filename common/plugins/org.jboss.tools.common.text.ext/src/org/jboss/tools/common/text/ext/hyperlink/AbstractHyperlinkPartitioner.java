739f3c177d7e353a12690af3ccde1cf4ab386199
Victor Rubezhny <ruby@exadel.com>
2007-08-16 19:34:13 +0000

    http://jira.jboss.com/jira/browse/JBIDE-735 Open-Ons for Beans and Bean properties don't work in Seam projects.
    A support is added for hiperlinkPartitioners ordering
    
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@3191 a97e2381-89e5-4abb-bab3-167db6db766c

--- common/plugins/org.jboss.tools.common.text.ext/src/org/jboss/tools/common/text/ext/hyperlink/AbstractHyperlinkPartitioner.java
+++ common/plugins/org.jboss.tools.common.text.ext/src/org/jboss/tools/common/text/ext/hyperlink/AbstractHyperlinkPartitioner.java
@@ -10,9 +10,14 @@
  ******************************************************************************/ 
 package org.jboss.tools.common.text.ext.hyperlink;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.runtime.IExtension;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.wst.sse.core.internal.provisional.IModelManager;
 import org.eclipse.wst.sse.core.internal.provisional.StructuredModelManager;
+import org.eclipse.wst.sse.ui.internal.util.Sorter;
 import org.jboss.tools.common.text.ext.util.StructuredModelWrapper;
 import org.jboss.tools.common.text.ext.util.Utils;
 import org.w3c.dom.Attr;
@@ -28,9 +33,18 @@ public abstract class AbstractHyperlinkPartitioner implements IHyperlinkPartitio
         IHyperlinkRegion childRegion = parse(document, superRegion);
         if (childRegion == null) return null;
         HyperlinkPartitionerDefinition[] hyperlinkPartitionerDefinitions = HyperlinkPartitionerBuilder.getInstance().getHyperlinkPartitionerDefinitions(childRegion.getContentType(), childRegion.getType(), childRegion.getAxis());
+
+        List<IHyperlinkPartitioner> partitioners = new ArrayList<IHyperlinkPartitioner>();
         for(int i=0; i<hyperlinkPartitionerDefinitions.length; i++) {
             HyperlinkPartitionerDefinition def = hyperlinkPartitionerDefinitions[i];
             IHyperlinkPartitioner partitioner = def.createHyperlinkPartitioner();
+            if (partitioner != null)
+            	partitioners.add(partitioner);
+        }
+        IHyperlinkPartitioner[] sortedPartitioners = orderHyperlinkPartitioners(partitioners.toArray(new IHyperlinkPartitioner[0])); 
+        
+        for(int i=0; sortedPartitioners != null && i<sortedPartitioners.length; i++) {
+            IHyperlinkPartitioner partitioner = sortedPartitioners[i];
             if((!(partitioner instanceof IHyperlinkPartitionRecognizer)) || 
             		((IHyperlinkPartitionRecognizer)partitioner).recognize(document, childRegion)) {
             	if (partitioner instanceof IExclusiblePartitionerRecognition) {
@@ -46,6 +60,41 @@ public abstract class AbstractHyperlinkPartitioner implements IHyperlinkPartitio
         return childRegion.getType();
     }
 
+	protected Sorter createSorter() {
+		return new Sorter() {
+			public boolean compare(Object partitioner1, Object partitioner2) {
+				IHyperlinkPartitioner p1 = (IHyperlinkPartitioner)partitioner1;
+				IHyperlinkPartitioner p2 = (IHyperlinkPartitioner)partitioner2;
+
+				String s1 = ((IHyperlinkPartitioner) partitioner1).getClass().getName();
+				String s2 = ((IHyperlinkPartitioner) partitioner2).getClass().getName();
+
+				int pp1 = Integer.MAX_VALUE;
+				int pp2 = Integer.MAX_VALUE;
+				
+				if (p1 instanceof IHyperLinkPartitionPriority)
+					pp1 = ((IHyperLinkPartitionPriority)p1).getPriority();
+
+				if (p2 instanceof IHyperLinkPartitionPriority)
+					pp2 = ((IHyperLinkPartitionPriority)p2).getPriority();
+				
+				if (pp1 == pp2) {
+					return s2.compareTo(s1) > 0;
+				}
+
+				return (pp1 < pp2);
+			}
+		};
+	}
+
+	protected IHyperlinkPartitioner[] orderHyperlinkPartitioners(IHyperlinkPartitioner[] partitioners) {
+		Object[] sorted = createSorter().sort(partitioners);
+		IHyperlinkPartitioner[] sortedPartitioners = new IHyperlinkPartitioner[sorted.length];
+		System.arraycopy(sorted, 0, sortedPartitioners, 0, sorted.length);
+		return sortedPartitioners;
+	}
+
+    
     IHyperlinkPartitioner findExclusionPartitioner (String partitionType, HyperlinkPartitionerDefinition[] hyperlinkPartitionerDefinitions, IDocument document, IHyperlinkRegion region) {
         for(int i=0; i<hyperlinkPartitionerDefinitions.length; i++) {
             HyperlinkPartitionerDefinition def = hyperlinkPartitionerDefinitions[i];
