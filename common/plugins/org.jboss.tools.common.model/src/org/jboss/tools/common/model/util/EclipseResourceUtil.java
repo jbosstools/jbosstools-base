8be9e1b70fe321cc1dbf69ad58280492574e3e41
Denis Golovin <dgolovin@exadel.com>
2007-11-10 01:05:19 +0000

    fix compilation poblem.
    Syntax error, insert ")" to complete VariableInitializer
    
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@4831 a97e2381-89e5-4abb-bab3-167db6db766c

--- common/plugins/org.jboss.tools.common.model/src/org/jboss/tools/common/model/util/EclipseResourceUtil.java
+++ common/plugins/org.jboss.tools.common.model/src/org/jboss/tools/common/model/util/EclipseResourceUtil.java
@@ -406,7 +406,7 @@ public class EclipseResourceUtil {
 			ArrayList<String> l = new ArrayList<String>();
 			for (int i = 0; i < es.length; i++) {
 				if(es[i].getEntryKind() != IClasspathEntry.CPE_SOURCE) continue;
-				IResource member = project.findMember(es[i].getPath().removeFirstSegments(1);
+				IResource member = project.findMember(es[i].getPath().removeFirstSegments(1));
 				if(member.exists()) { // The member might not exist!
 					l.add(member.getLocation().toString());
 				}
