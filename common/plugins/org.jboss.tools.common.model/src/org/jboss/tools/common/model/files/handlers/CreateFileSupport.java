52d54d022a508b0ccf7bd821a9ce562b77fe0772
Viacheslav Kabanovich <scabanovich@exadel.com>
2008-01-22 12:29:28 +0000

    JBIDE-1474
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@5861 a97e2381-89e5-4abb-bab3-167db6db766c

--- common/plugins/org.jboss.tools.common.model/src/org/jboss/tools/common/model/files/handlers/CreateFileSupport.java
+++ common/plugins/org.jboss.tools.common.model/src/org/jboss/tools/common/model/files/handlers/CreateFileSupport.java
@@ -34,6 +34,7 @@ public class CreateFileSupport extends SpecialWizardSupport {
 	boolean useVersions = false;
 	
 	public void reset() {
+		targetHolder.setAction(action);
 		if(hasTemplate()) {
 			String[] s = getPageTemplateList();
 			setValueList(0, "template", s);
@@ -279,53 +280,6 @@ public class CreateFileSupport extends SpecialWizardSupport {
 		return targetHolder.target;
 	}
 	
-	class TargetHolder {
-		XModelObject target;
-		String path;
-		IResource folder;
-		String addPath = "";
-		
-		public void revalidate(String newPath) {
-			if(newPath == path || (newPath != null && newPath.equals(path))) return;
-			path = newPath;
-			addPath = "";
-			folder = (path == null) ? null : ModelPlugin.getWorkspace().getRoot().findMember(path);
-			target = EclipseResourceUtil.getObjectByResource(folder);
-			if(path != null && (folder == null || !folder.exists())) {
-				String p = path.replace('\\', '/');
-				String ap = "";
-				while(true) {
-					int q = p.lastIndexOf('/');
-					if(q < 0) break;
-					ap = p.substring(q) + ap;
-					p = p.substring(0, q);
-					folder = ModelPlugin.getWorkspace().getRoot().findMember(p);
-					if(folder != null && folder.exists()) {
-						addPath = ap;
-						if(addPath.endsWith("/")) addPath = addPath.substring(0, addPath.length() - 1);
-						target = EclipseResourceUtil.createObjectForResource(folder);
-						break;
-					}
-				}
-			} else if(target == null && folder != null && folder.exists()) {
-				target = EclipseResourceUtil.createObjectForResource(folder);
-			} else if(target != null && "FileSystems".equals(target.getModelEntity().getName())) {
-				target = EclipseResourceUtil.findFileSystem(folder, target.getModel());
-				if(target == null) target = EclipseResourceUtil.createObjectForResource(folder);
-			}
-		}
-		public void saveLastPath() {
-			if(path == null || folder == null) return;
-			QualifiedName n = new QualifiedName("", action.getName() + "_lastPath");
-			try {
-				folder.getProject().setPersistentProperty(n, path);
-			} catch (Exception e) {
-				ModelPlugin.getPluginLog().logError("CreateFileSuppport:TargetHolder:saveLastPath:" + e.getMessage());
-			}
-		}
-		
-	}
-
 	protected DefaultWizardDataValidator validator = createValidator();
     
 	public WizardDataValidator getValidator(int step) {
