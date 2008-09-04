84f3b795e33935b093231cd0e1491f91dd829b90
Denis Golovin <dgolovin@exadel.com>
2008-09-04 23:17:09 +0000

    Share project "org.jboss.tools.common.resref.core" into "https://svn.jboss.org/repos/jbosstools" Share project "org.jboss.tools.common.resref.ui" into "https://svn.jboss.org/repos/jbosstools"
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@10077 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ common/plugins/org.jboss.tools.common.resref.core/src/org/jboss/tools/common/resref/core/ResourceReferenceListListener.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2007 Exadel, Inc. and Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Exadel, Inc. and Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/ 
+package org.jboss.tools.common.resref.core;
+
+import org.eclipse.core.runtime.IPath;
+
+public interface ResourceReferenceListListener {
+	public IPath getPath();
+	public void changed(Object source);
+	
+}
