9e71b0ee7be86deab637de11f3d01b4743d11a34
Denis Golovin <dgolovin@exadel.com>
2008-09-04 23:14:39 +0000

    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@10074 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ common/plugins/org.jboss.tools.common.el.core/src/org/jboss/tools/common/el/vpe/ELReferenceList.java
@@ -0,0 +1,36 @@
+package org.jboss.tools.common.el.vpe;
+
+
+import org.eclipse.core.runtime.QualifiedName;
+import org.jboss.tools.common.resref.core.ResourceReferenceList;
+
+
+/**
+ * The Class ELReferenceList.
+ */
+public class ELReferenceList extends ResourceReferenceList {
+    
+    /** The PROPERT y_ NAME. */
+    private static QualifiedName PROPERTY_NAME = new QualifiedName("", "org.jboss.tools.vpe.editor.css.ELReference");
+
+    /** The instance. */
+    static ELReferenceList instance = new ELReferenceList();
+
+    /**
+     * Gets the instance.
+     * 
+     * @return the instance
+     */
+    public static ELReferenceList getInstance() {
+        return instance;
+    }
+
+    /**
+     * Gets the property name.
+     * 
+     * @return the property name
+     */
+    protected QualifiedName getPropertyName() {
+        return PROPERTY_NAME;
+    }
+}
