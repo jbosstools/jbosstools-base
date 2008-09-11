c5f05cbca5372ff5ff244e4479e832b2e26cbcf9
Viacheslav Kabanovich <scabanovich@exadel.com>
2008-09-11 13:18:48 +0000

    JBIDE-2025
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@10203 a97e2381-89e5-4abb-bab3-167db6db766c

--- common/plugins/org.jboss.tools.common.model.ui/src/org/jboss/tools/common/model/ui/ModelUIPlugin.java
+++ common/plugins/org.jboss.tools.common.model.ui/src/org/jboss/tools/common/model/ui/ModelUIPlugin.java
@@ -15,19 +15,9 @@ import java.util.ResourceBundle;
 
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Platform;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.jdt.internal.ui.IJavaStatusConstants;
-import org.eclipse.jdt.internal.ui.JavaPlugin;
-import org.eclipse.jdt.ui.JavaUI;
-import org.eclipse.jdt.ui.PreferenceConstants;
-import org.eclipse.jface.preference.IPreferenceStore;
-import org.eclipse.jface.text.templates.TemplateContextType;
 import org.jboss.tools.common.log.BaseUIPlugin;
 import org.jboss.tools.common.log.IPluginLog;
 import org.jboss.tools.common.model.plugin.IModelPlugin;
-import org.jboss.tools.common.model.ui.templates.ExtendedTemplateContextType;
 import org.osgi.framework.BundleContext;
 
 public class ModelUIPlugin extends BaseUIPlugin implements IModelPlugin {
@@ -64,17 +54,6 @@ public class ModelUIPlugin extends BaseUIPlugin implements IModelPlugin {
 	
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
-		//configureJavaPlugin();
-	}
-
-	/*private void configureJavaPlugin() {
-		TemplateContextType contextType = new ExtendedTemplateContextType();
-		JavaPlugin.getDefault().getTemplateContextRegistry().addContextType(contextType);
-	}*/
-	
-	protected void initializeDefaultPluginPreferences() {		
-		Platform.getBundle(JavaUI.ID_PLUGIN);		
-		PreferenceConstants.initializeDefaultValues(getPreferenceStore());
 	}
 
 	public static ModelUIPlugin getDefault() {
@@ -87,14 +66,9 @@ public class ModelUIPlugin extends BaseUIPlugin implements IModelPlugin {
 	
 
     public static final String PROJECT_OVERRIDE = "org.jboss.tools.common.model.ui.templates.projectOverriveTemplates";
-    public static final String PROJECT_CAN_OVERRIDE = "org.jboss.tools.common.model.ui.templates.projectCanOverriveTemplates";
+//    public static final String PROJECT_CAN_OVERRIDE = "org.jboss.tools.common.model.ui.templates.projectCanOverriveTemplates";
 	public static final String ID_PLUGIN= "org.jboss.tools.common.model.ui.templates";
 	public static final String META_TEMPLATES_DTD = "-//Red Hat Inc.//DTD Meta Templates 1.0//EN";
-
-    protected void initializeDefaultPreferences(IPreferenceStore store) {
-        store.setDefault(PROJECT_CAN_OVERRIDE, false);
-        super.initializeDefaultPreferences(store);
-    }
 
 	public static String getPluginId() {
 		return ModelUIPlugin.ID_PLUGIN;
