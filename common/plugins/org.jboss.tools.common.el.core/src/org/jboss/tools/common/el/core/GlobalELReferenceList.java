4191294032bd777786ae197c0b801f02e8691634
Max Rydahl Andersen <max@hibernate.org>
2008-09-17 09:54:39 +0000

    whoops - rollback accidential delete of src directories (JBIDE-2177)
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@10308 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ common/plugins/org.jboss.tools.common.el.core/src/org/jboss/tools/common/el/core/GlobalELReferenceList.java
@@ -0,0 +1,51 @@
+/******************************************************************************* 
+ * Copyright (c) 2007 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+
+package org.jboss.tools.common.el.core;
+
+import org.eclipse.core.runtime.QualifiedName;
+import org.jboss.tools.common.resref.core.ResourceReferenceList;
+
+/**
+ * @author Evgenij Stherbin
+ *
+ */
+public class GlobalELReferenceList extends ResourceReferenceList {
+    /** The PROPERT y_ NAME. */
+    private static QualifiedName PROPERTY_NAME = new QualifiedName("", "org.jboss.tools.vpe.editor.css.GlobalELReference");
+
+    /** The instance. */
+    private static GlobalELReferenceList instance = new GlobalELReferenceList();
+
+    /**
+     * Gets the instance.
+     * 
+     * @return the instance
+     */
+    public synchronized static GlobalELReferenceList getInstance() {
+        return instance;
+    }
+
+    private GlobalELReferenceList() {
+        super();
+    }
+
+    /**
+     * Gets the property name.
+     * 
+     * @return the property name
+     */
+    protected QualifiedName getPropertyName() {
+        return PROPERTY_NAME;
+    }
+
+
+}
