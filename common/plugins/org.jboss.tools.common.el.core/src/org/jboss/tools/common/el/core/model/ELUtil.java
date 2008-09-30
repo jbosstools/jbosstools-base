b03dec08ab318beaf8f52209ce27a43a13e3b065
Viacheslav Kabanovich <scabanovich@exadel.com>
2008-09-30 09:58:19 +0000

    JBIDE-1497. Minor improvements
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@10554 a97e2381-89e5-4abb-bab3-167db6db766c

--- common/plugins/org.jboss.tools.common.el.core/src/org/jboss/tools/common/el/core/model/ELUtil.java
+++ common/plugins/org.jboss.tools.common.el.core/src/org/jboss/tools/common/el/core/model/ELUtil.java
@@ -12,7 +12,24 @@ package org.jboss.tools.common.el.core.model;
 
 import java.util.List;
 
+import org.jboss.tools.common.el.core.parser.LexicalToken;
+
 public class ELUtil {
+
+	public static ELInstance findInstance(ELModel model, int offset) {
+		if(model == null) return null;
+		List<ELInstance> is = model.getInstances();
+		for (ELInstance i: is) {
+			if(i.getStartPosition() >= offset) continue;
+			LexicalToken c = i.getCloseInstanceToken();
+			if(c != null) {
+				if(c.getStart() >= offset) return i;
+			} else {
+				if(i.getEndPosition() >= offset) return i;
+			}
+		}
+		return null;
+	}
 
 	public static ELInvocationExpression findExpression(ELModel model, int offset) {
 		ELInvocationExpression result = null;
