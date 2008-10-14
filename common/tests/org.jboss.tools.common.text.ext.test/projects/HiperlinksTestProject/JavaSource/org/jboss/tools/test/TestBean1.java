ab4fa9f618c4a774ef8eaea981324179d7369b46
Denis Golovin <dgolovin@exadel.com>
2008-10-14 05:09:02 +0000

    fix test errors
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@10819 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ common/tests/org.jboss.tools.common.text.ext.test/projects/HiperlinksTestProject/JavaSource/org/jboss/tools/test/TestBean1.java
@@ -0,0 +1,36 @@
+package org.jboss.tools.test;
+
+public class TestBean1 {
+	
+	private String property1;
+	
+	private String property2;
+	
+	private int property3;
+	
+	public String getProperty1() {
+		return property1;
+	}
+	
+	public void setProperty1(String property1) {
+		this.property1 = property1;
+	}
+	
+	public String getProperty2() {
+		return property2;
+	}
+	
+	public void setProperty2(String property2) {
+		this.property2 = property2;
+	}
+	
+	public int getProperty3() {
+		return property3;
+	}
+	
+	public void setProperty3(int property3) {
+		this.property3 = property3;
+	}
+	
+	
+}
