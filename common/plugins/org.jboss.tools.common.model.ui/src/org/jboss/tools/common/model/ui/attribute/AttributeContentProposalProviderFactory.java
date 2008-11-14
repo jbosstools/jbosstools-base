54b5b77a83660f284277b7251dacc66517a578fa
Viacheslav Kabanovich <scabanovich@exadel.com>
2008-11-14 13:53:08 +0000

    JBIDE-2575
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@11786 a97e2381-89e5-4abb-bab3-167db6db766c

--- common/plugins/org.jboss.tools.common.model.ui/src/org/jboss/tools/common/model/ui/attribute/AttributeContentProposalProviderFactory.java
+++ common/plugins/org.jboss.tools.common.model.ui/src/org/jboss/tools/common/model/ui/attribute/AttributeContentProposalProviderFactory.java
@@ -1,8 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2007 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/ 
 package org.jboss.tools.common.model.ui.attribute;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtension;
+import org.eclipse.core.runtime.IExtensionPoint;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.bindings.keys.KeyStroke;
 import org.eclipse.jface.bindings.keys.ParseException;
 import org.eclipse.jface.fieldassist.ComboContentAdapter;
@@ -21,6 +36,9 @@ import org.jboss.tools.common.model.XModelObject;
 import org.jboss.tools.common.model.ui.ModelUIPlugin;
 import org.jboss.tools.common.model.ui.attribute.adapter.DefaultValueAdapter;
 
+/**
+ * @author Viacheslav Kabanovich
+ */
 public class AttributeContentProposalProviderFactory {
 	private static List<IAttributeContentProposalProvider> EMPTY = new ArrayList<IAttributeContentProposalProvider>();
 
@@ -79,21 +97,52 @@ public class AttributeContentProposalProviderFactory {
 		}
 	}
 
+	static String POINT_ID = ModelUIPlugin.PLUGIN_ID + ".attributeContentProposalProviders";
+
 	public List<IAttributeContentProposalProvider> getContentProposalProviders(XModelObject object, XAttribute attribute) {
-		//TODO
-		if(true) {
-			List<IAttributeContentProposalProvider> list = new ArrayList<IAttributeContentProposalProvider>();
-			list.add(new TestAttributeContentProposalProvider());
-			return list;
+		List<IAttributeContentProposalProvider> result = EMPTY;
+
+		IExtensionPoint point = Platform.getExtensionRegistry().getExtensionPoint(POINT_ID);
+		
+		IExtension[] es = point.getExtensions();
+		for (int i = 0; i < es.length; i++) {
+			IConfigurationElement[] cs = es[i].getConfigurationElements();
+			for (int j = 0; j < cs.length; j++) {
+				IAttributeContentProposalProvider provider = null;
+				try {
+					Object o = cs[j].createExecutableExtension("class");
+					provider = (IAttributeContentProposalProvider)o;
+				} catch (CoreException e) {
+					ModelUIPlugin.getPluginLog().logError(e);
+				}
+				if(provider.isRelevant(object, attribute)) {
+					if(result == EMPTY) result = new ArrayList<IAttributeContentProposalProvider>();
+					result.add(provider);
+				}
+			}
+		}
+
+		if(false) {
+			//Test
+			result.add(new TestAttributeContentProposalProvider());
 		}
 		
-		return EMPTY;
+		return result;
 	}
 }
 
+/**
+ * Example
+ * @author glory
+ *
+ */
 class TestAttributeContentProposalProvider implements IAttributeContentProposalProvider {
 
-	public void dispose() {
+	public boolean isRelevant(XModelObject object, XAttribute attribute) {
+		return true;
+	}
+
+	public void init(XModelObject object, XAttribute attribute) {
 	}
 
 	public IContentProposalProvider getContentProposalProvider() {
@@ -103,8 +152,9 @@ class TestAttributeContentProposalProvider implements IAttributeContentProposalP
 					int position) {
 				ArrayList<IContentProposal> ps = new ArrayList<IContentProposal>();
 				
-				if(position <= contents.length() && position > 0 && 'b' == contents.charAt(position - 1))
-				ps.add(makeContentProposal("aaa", ".aaa"));
+				if(position <= contents.length() && position > 3 && "test".equals(contents.substring(position - 4, position))) {
+					ps.add(makeContentProposal("aaa", ".aaa"));
+				}
 				
 				return ps.toArray(new IContentProposal[0]);
 			}
@@ -117,7 +167,7 @@ class TestAttributeContentProposalProvider implements IAttributeContentProposalP
 		return ContentProposalAdapter.PROPOSAL_INSERT;
 	}
 
-	public void init(XModelObject object, XAttribute attribute) {
+	public void dispose() {
 	}
 
 	private IContentProposal makeContentProposal(final String proposal, final String label) {
--- common/plugins/org.jboss.tools.common.model.ui/src/org/jboss/tools/common/model/ui/attribute/IAttributeContentProposalProvider.java
+++ common/plugins/org.jboss.tools.common.model.ui/src/org/jboss/tools/common/model/ui/attribute/IAttributeContentProposalProvider.java
@@ -1,10 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2007 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/ 
 package org.jboss.tools.common.model.ui.attribute;
 
 import org.eclipse.jface.fieldassist.IContentProposalProvider;
 import org.jboss.tools.common.meta.XAttribute;
 import org.jboss.tools.common.model.XModelObject;
 
+/**
+ * @author Viacheslav Kabanovich
+ */
 public interface IAttributeContentProposalProvider {
+
+	public boolean isRelevant(XModelObject object, XAttribute attribute);
 
 	public void init(XModelObject object, XAttribute attribute);
 
