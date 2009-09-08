f5f77253d39a183871dda6d3cccbc9d43acbef9a
Denis Golovin <dgolovin@exadel.com>
2009-09-08 23:45:58 +0000

    https://jira.jboss.org/jira/browse/JBIDE-3892 increase code coverage for o.j.t.common plug-in
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@17482 a97e2381-89e5-4abb-bab3-167db6db766c

--- common/tests/org.jboss.tools.common.test/src/org/jboss/tools/common/xml/test/XMLUtilitiesTest.java
+++ common/tests/org.jboss.tools.common.test/src/org/jboss/tools/common/xml/test/XMLUtilitiesTest.java
@@ -10,7 +10,14 @@
  ******************************************************************************/
 package org.jboss.tools.common.xml.test;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import javax.xml.parsers.DocumentBuilder;
+
 import org.jboss.tools.common.xml.XMLUtilities;
+import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 import junit.framework.TestCase;
@@ -18,8 +25,7 @@ import junit.framework.TestCase;
 public class XMLUtilitiesTest extends TestCase {
 
 	public final void testCreateDocumentBuilder() {
-		assertNotNull("Element builder without validation wasn't created", XMLUtilities.createDocumentBuilder(true)); // TODO
-		assertNotNull("Element builder with validation wasn't created", XMLUtilities.createDocumentBuilder(true)); // TODO		
+		assertNotNull("Element builder without validation wasn't created", XMLUtilities.createDocumentBuilder()); // TODO
 	}
 
 	public static final String ELEMENT_NAME = "ElementName";
@@ -30,4 +36,56 @@ public class XMLUtilitiesTest extends TestCase {
 		assertTrue(element.getNodeName().equals(ELEMENT_NAME));
 	}
 
+	public void testCreateDocumentBuilderBoolean() {
+		assertNotNull("Element builder with validation wasn't created", XMLUtilities.createDocumentBuilder(true)); // TODO
+	}
+	
+	public void testGetDocumentReaderEntityResolver() throws IOException {
+		assertNotNull(getGoodDocument());;
+	}
+	
+	public void testHasAttributeElementString() {
+		Element element = getGoodDocument().getDocumentElement();
+		assertTrue(XMLUtilities.hasAttribute(element, "attribute1"));
+	}
+	
+	public void testGetChildrenElementsElementString() {
+		Element element = getGoodDocument().getDocumentElement();
+		assertTrue(XMLUtilities.getChildren(element, "child1").length==4);
+	}
+	
+	public void testGetUniqueChildElementString() {
+		Element element = getGoodDocument().getDocumentElement();
+		assertNotNull(XMLUtilities.getUniqueChild(element, "child1"));
+	}
+	
+	public void testGetChildrenElementString() {
+		Element element = getGoodDocument().getDocumentElement();
+		assertTrue(XMLUtilities.getChildren(element, "child1").length==4);
+	}
+	
+	public void testGetFirstChildElementString() {
+		Element element = getGoodDocument().getDocumentElement();
+		Element first = XMLUtilities.getFirstChild(element,"child1");
+		assertTrue(first.getAttribute("first").equals("true"));
+		first = XMLUtilities.getFirstChild(element,"child2");
+		assertTrue(first.getAttribute("first").equals("true"));
+		first = XMLUtilities.getFirstChild(element,"child10");
+		assertNull(first);
+	}
+	
+	public static final Document getGoodDocument() {
+		InputStream in = null;
+		try {
+			in = XMLUtilitiesTest.class.getResourceAsStream("XMLUtilitiesTest1.xml");
+			Document document = XMLUtilities.getDocument(new InputStreamReader(in), XMLUtilities.EMPTY_RESOLVER);
+			assertNotNull(document);
+			return document;
+		} finally {
+			try {
+				in.close();
+			} catch (IOException e) {
+			}
+		}
+	}
 }
