b340ae39fa5052ce7a8dd27cec665d02a9617b58
Denis Golovin <dgolovin@exadel.com>
2009-09-16 07:55:52 +0000

    fix JUnit test error in CommonAllTests.testMethodNotImplementedException
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@17608 a97e2381-89e5-4abb-bab3-167db6db766c

--- common/plugins/org.jboss.tools.common/src/org/jboss/tools/common/MethodNotImplementedException.java
+++ common/plugins/org.jboss.tools.common/src/org/jboss/tools/common/MethodNotImplementedException.java
@@ -17,7 +17,7 @@ package org.jboss.tools.common;
 public class MethodNotImplementedException extends RuntimeException {
 
 	public MethodNotImplementedException() {
-		super("Method is not implemented yet"); //$NON-NLS-1$
+		super(Messages.MethodNotImplementedException_MethodIsNotImplementedYet); //$NON-NLS-1$
 	}
 
 	public MethodNotImplementedException(String message, Throwable cause) {
