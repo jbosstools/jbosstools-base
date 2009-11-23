42f9d4566188ba00dff9b7bec4ad71040e0651a9
Maxim Areshkau <mareshkau@exadel.com>
2009-11-23 18:23:29 +0000

    https://jira.jboss.org/jira/browse/JBIDE-4635
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@18790 a97e2381-89e5-4abb-bab3-167db6db766c

--- common/plugins/org.jboss.tools.common.text.ext/src/org/jboss/tools/common/text/ext/hyperlink/xml/FaceletSourceTagHyperlink.java
+++ common/plugins/org.jboss.tools.common.text.ext/src/org/jboss/tools/common/text/ext/hyperlink/xml/FaceletSourceTagHyperlink.java
@@ -10,13 +10,29 @@
  ******************************************************************************/
 package org.jboss.tools.common.text.ext.hyperlink.xml;
 
+import java.util.zip.ZipEntry;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IStorage;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.internal.core.JarEntryFile;
+import org.eclipse.jdt.internal.core.JarPackageFragmentRoot;
 import org.eclipse.jdt.internal.ui.javaeditor.JarEntryEditorInput;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IStorageEditorInput;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PlatformUI;
+import org.jboss.tools.common.text.ext.ExtensionsPlugin;
 import org.jboss.tools.common.text.ext.hyperlink.LinkHyperlink;
+
 
 /**
  * @author mareshkau
@@ -48,9 +64,86 @@ public class FaceletSourceTagHyperlink extends LinkHyperlink{
 					openFileFailed();
 				}
 			}
-		} else { 
-			super.doHyperlink(region);
-		}
+			return;
+		} 
+		else if(editorPart.getEditorInput() instanceof IStorageEditorInput) {
+			IStorageEditorInput modelObjectStorageEditorInput = 
+				(IStorageEditorInput)	editorPart.getEditorInput(); 
+			try {
+				
+				IStorage storage = modelObjectStorageEditorInput.getStorage();
+				if(storage!=null && getFilePath(region)!=null) {
+					if(openOnFromModelEditorIntup(storage.getFullPath(), getFilePath(region))){
+						return;
+					}
+				}
+			} catch (CoreException e) {
+				ExtensionsPlugin.getDefault().logError(e);
+			}
+		} 
+		super.doHyperlink(region);
 	}
-	
+	/**
+	 * 
+	 * @param xmodelPaht it's path to file in jar archive on which we are trying to make open on
+	 * Example 'JBIDE3247/FileSystems/lib-mydomain.jar/META-INF/custom.taglib.xml'
+	 * @param fileToOpen jar archive relative name for which we are trying to make open on
+	 * Example '/inputCurrency.xhtml's
+	 */
+	private boolean openOnFromModelEditorIntup(IPath xmodelPath, String fileToOpen) {
+		if(xmodelPath==null || fileToOpen==null) {
+			return false;
+		}
+		String projectName = xmodelPath.segment(0);
+		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
+		JarEntryEditorInput  editorInputToOpenEditor=seachResourceInClassPath(project,new Path(fileToOpen.substring(1)),xmodelPath);
+		if(editorInputToOpenEditor==null) {
+			return false;
+		}
+		IEditorPart openedEditor = openFileInEditor(editorInputToOpenEditor, 
+				editorInputToOpenEditor.getName());
+		if(openedEditor==null) {
+			openFileFailed();
+			return false;
+		} 
+		return true;
+	}
+	/**
+	 * 
+	 * @param project project from which we are going to open File
+	 * @param classPathResource resource relativy to class path
+	 * @param xmodelPath xmodel path to file
+	 * @return
+	 */
+    private static JarEntryEditorInput  seachResourceInClassPath(IProject project, IPath classPathResource, IPath xmodelPath) {
+		IJavaProject javaProject = JavaCore.create(project);
+		try {
+			for (IPackageFragmentRoot fragmentRoot : javaProject.getAllPackageFragmentRoots()) {
+				if(fragmentRoot instanceof JarPackageFragmentRoot) {
+					JarPackageFragmentRoot jarPackageFragmentRoot = (JarPackageFragmentRoot) fragmentRoot;
+					ZipEntry zipEntry = jarPackageFragmentRoot.getJar().getEntry(classPathResource.toString());
+					//the name of jar file shoul be in xmodel path
+					//for now if model have following
+					if(zipEntry!=null&&xmodelPath.toString().contains(fragmentRoot.getElementName())){
+						JarEntryFile fileInJar = new JarEntryFile(classPathResource.lastSegment());									
+						Object parent = null;
+						String parentName =classPathResource.removeLastSegments(1).toString();
+						if(parentName.length()==0) {
+							parent = jarPackageFragmentRoot;
+						}else{	
+							parent = jarPackageFragmentRoot.getPackageFragment(classPathResource.removeLastSegments(1).toString());
+						}
+						fileInJar.setParent(parent);
+						JarEntryEditorInput jarEditorInput = new JarEntryEditorInput(fileInJar);
+						return jarEditorInput;
+					}
+				}
+			}
+		} catch (JavaModelException e) {
+			ExtensionsPlugin.getDefault().logError(e);
+		} catch (CoreException e) {
+			ExtensionsPlugin.getDefault().logError(e);
+		} 
+    	return null;
+    }
 }
