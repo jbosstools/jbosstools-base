651b2ebbef91eefa9fd42c59327442104d1028fd
Alexey Kazakov <akazakov@exadel.com>
2010-05-13 15:03:19 +0000

    https://jira.jboss.org/jira/browse/JBIDE-2708
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@22067 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/plugins/org.jboss.tools.tests/src/org/jboss/tools/tests/AbstractResourceMarkerTest.java
+++ tests/plugins/org.jboss.tools.tests/src/org/jboss/tools/tests/AbstractResourceMarkerTest.java
@@ -65,9 +65,10 @@ public class AbstractResourceMarkerTest extends TestCase {
 	public static IMarker[] findMarkers(IResource resource, String type, String pattern) throws CoreException {
 		List<IMarker> result = new ArrayList<IMarker>();
 		IMarker[] markers = resource.findMarkers(type, true, IResource.DEPTH_INFINITE);
+		System.out.println("");
 		for (int i = 0; i < markers.length; i++) {
 			String message = markers[i].getAttribute(IMarker.MESSAGE, ""); //$NON-NLS-1$
-			if (message.matches(pattern) && markers[i].exists()) {
+			if ((message.matches(pattern)||message.equals(pattern)) && markers[i].exists()) {
 				result.add(markers[i]);
 			}
 		}
