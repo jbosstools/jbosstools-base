099c712742b279ac243ade7f8a43431915b452b5
Denis Golovin <dgolovin@exadel.com>
2010-05-26 22:35:57 +0000

    https://jira.jboss.org/browse/JBDS-1202 problems running tests ResourceUtils importProject distinguish import form plug-in distributed as jar or unpacked jar
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@22348 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ tests/plugins/org.jboss.tools.tests/src/org/jboss/tools/tests/TestsPlugin.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.jboss.tools.tests;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+/**
+ * @author eskimo
+ *
+ */
+public class TestsPlugin extends AbstractUIPlugin {
+
+	
+	public static final String ID = "org.jboss.tools.tests"; 
+	/**
+	 * 
+	 */
+	public TestsPlugin() {
+		
+	}
+
+}
