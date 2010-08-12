df1d0d1eaf7cc08ea2a9661f4e1b15722a756266
Nick Boldt <nboldt@redhat.com>
2010-08-12 16:21:30 +0000

    JBIDE-6300 move from tests/ into common/ to resolve cyclic dependency
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@24111 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/plugins/org.jboss.tools.ui.bot.ext.test/src/org/jboss/tools/ui/bot/ext/test/config/ClassWithoutTests.java
+++
@@ -1,8 +0,0 @@
-package org.jboss.tools.ui.bot.ext.test.config;
-
-import org.jboss.tools.ui.bot.ext.config.Annotations.SWTBotTestRequires;
-
-@SWTBotTestRequires()
-public class ClassWithoutTests {
-
-}
