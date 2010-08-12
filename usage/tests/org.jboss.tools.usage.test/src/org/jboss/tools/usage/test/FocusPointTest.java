49f63819bab1f008874028fcfd1651b41ad2d78f
Andre Dietisheim <adietish@redhat.com>
2010-08-12 12:05:58 +0000

    [JBIDE-6376] focuspoint & test fixed
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@24093 a97e2381-89e5-4abb-bab3-167db6db766c

--- usage/tests/org.jboss.tools.usage.test/src/org/jboss/tools/usage/test/FocusPointTest.java
+++ usage/tests/org.jboss.tools.usage.test/src/org/jboss/tools/usage/test/FocusPointTest.java
@@ -1,6 +1,7 @@
 package org.jboss.tools.usage.test;
 
 import org.jboss.tools.usage.jgoogleanalytics.FocusPoint;
+import org.jboss.tools.usage.util.EncodingUtils;
 
 import junit.framework.TestCase;
 
@@ -8,20 +9,20 @@ public class FocusPointTest extends TestCase {
 
 	private static final String root = "root";
 	private static final String child1 = "child1";
-	private static final String child2 = "child2";
+	private static final String URI_SEPARATOR_ENCODED = EncodingUtils.checkedEncodeUtf8(FocusPoint.URI_SEPARATOR);
 	
 	public void testGetContentURI_Simple() throws Exception {
 		FocusPoint focusPoint = new FocusPoint(root);
 		String contentURI = focusPoint.getContentURI();
 		assertNotNull(contentURI);
-		assertEquals(FocusPoint.URI_SEPARATOR + root, contentURI);
+		assertEquals(URI_SEPARATOR_ENCODED + root, contentURI);
 	}
 
 	public void testGetContentURI_OneLevel() throws Exception {
 		FocusPoint focusPoint = new FocusPoint(root).setChild(new FocusPoint(child1));
 		String contentURI = focusPoint.getContentURI();
 		assertNotNull(contentURI);
-		assertEquals(FocusPoint.URI_SEPARATOR + root + FocusPoint.URI_SEPARATOR + child1, contentURI);
+		assertEquals(URI_SEPARATOR_ENCODED + root + URI_SEPARATOR_ENCODED + child1, contentURI);
 	}
 
 	public void testGetContentTitle_Simple() throws Exception {
@@ -29,14 +30,5 @@ public class FocusPointTest extends TestCase {
 		String contentTitle = focusPoint.getContentTitle();
 		assertNotNull(contentTitle);
 		assertEquals(root, contentTitle);
-	}
-
-	public void testGetContentTitle_OneLevel() throws Exception {
-//		FocusPoint parentFocusPoint = new FocusPoint(JBOSS_TOOLS_USAGE_ID);
-//		FocusPoint childFocusPoint = new FocusPoint(JBOSS_TOOLS_SMOOKS_UI,
-//				parentFocusPoint);
-//		String contentTitle = childFocusPoint.getContentTitle();
-//		assertNotNull(contentTitle);
-//		assertEquals(JBOSS_TOOLS_USAGE_ID + FocusPoint.TITLE_SEPARATOR + JBOSS_TOOLS_SMOOKS_UI, contentTitle);
 	}
 }
