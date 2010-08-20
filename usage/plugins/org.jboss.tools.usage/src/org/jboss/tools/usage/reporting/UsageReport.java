426713354a9b953ac70a975953cc63148f6ed09b
Andre Dietisheim <adietish@redhat.com>
2010-08-20 16:05:54 +0000

    [JBIDE-6376] refactored to separate from startup extension (might be better to use another activation later)
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@24334 a97e2381-89e5-4abb-bab3-167db6db766c

--- usage/plugins/org.jboss.tools.usage/src/org/jboss/tools/usage/reporting/UsageReport.java
+++ usage/plugins/org.jboss.tools.usage/src/org/jboss/tools/usage/reporting/UsageReport.java
@@ -10,6 +10,9 @@
  ******************************************************************************/
 package org.jboss.tools.usage.reporting;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.jface.window.Window;
+import org.eclipse.ui.PlatformUI;
 import org.jboss.tools.usage.FocusPoint;
 import org.jboss.tools.usage.ILoggingAdapter;
 import org.jboss.tools.usage.ITracker;
@@ -19,6 +22,9 @@ import org.jboss.tools.usage.Tracker;
 import org.jboss.tools.usage.googleanalytics.GoogleAnalyticsUrlStrategy;
 import org.jboss.tools.usage.googleanalytics.IGoogleAnalyticsParameters;
 import org.jboss.tools.usage.internal.JBossToolsUsageActivator;
+import org.jboss.tools.usage.util.PreferencesUtils;
+import org.jboss.tools.usage.util.StatusUtils;
+import org.osgi.service.prefs.BackingStoreException;
 
 /**
  * @author Andre Dietisheim
@@ -35,8 +41,31 @@ public class UsageReport {
 							.setChild(new FocusPoint("wsstartup")))); //$NON-NLS-1$
 
 	public void report() {
+		UsageReportEnablementDialog dialog = new UsageReportEnablementDialog(ReportingMessages.UsageReport_DialogTitle,
+				ReportingMessages.UsageReport_DialogMessage,
+				ReportingMessages.UsageReport_Checkbox_Text,
+				true,
+				PlatformUI.getWorkbench().getActiveWorkbenchWindow());
+		if (UsageReportPreferences.isAskUser()) {
+			if (dialog.open() == Window.OK) {
+				UsageReportPreferences.setEnabled(dialog.isReportEnabled());
+				UsageReportPreferences.setAskUser(false);
+				flushPreferences();
+			}
+		}
+
 		if (UsageReportPreferences.isEnabled()) {
-			report(getAnalyticsTracker());
+			report(getTracker());
+		}
+	}
+
+	private void flushPreferences() {
+		try {
+			UsageReportPreferences.flush();
+		} catch (BackingStoreException e) {
+			IStatus status = StatusUtils.getErrorStatus(JBossToolsUsageActivator.PLUGIN_ID,
+					ReportingMessages.UsageReport_Error_SavePreferences, e);
+			JBossToolsUsageActivator.getDefault().getLog().log(status);
 		}
 	}
 
@@ -44,11 +73,12 @@ public class UsageReport {
 		tracker.trackAsynchronously(focusPoint);
 	}
 
-	private ITracker getAnalyticsTracker() {
+	private ITracker getTracker() {
 		IGoogleAnalyticsParameters eclipseSettings = new EclipseEnvironment(
 				GANALYTICS_ACCOUNTNAME
 				, HOST_NAME
-				, IGoogleAnalyticsParameters.VALUE_NO_REFERRAL);
+				, IGoogleAnalyticsParameters.VALUE_NO_REFERRAL
+				, PreferencesUtils.getPreferences());
 		ILoggingAdapter loggingAdapter = new PluginLogger(JBossToolsUsageActivator.getDefault());
 		IURLBuildingStrategy urlStrategy = new GoogleAnalyticsUrlStrategy(eclipseSettings);
 		return new Tracker(urlStrategy, eclipseSettings.getUserAgent(), loggingAdapter);
--- usage/plugins/org.jboss.tools.usage/src/org/jboss/tools/usage/reporting/UsageReportDispatcher.java
+++ usage/plugins/org.jboss.tools.usage/src/org/jboss/tools/usage/reporting/UsageReportDispatcher.java
@@ -10,15 +10,8 @@
  ******************************************************************************/
 package org.jboss.tools.usage.reporting;
 
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.jface.window.Window;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IStartup;
-import org.eclipse.ui.PlatformUI;
-import org.jboss.tools.usage.internal.JBossToolsUsageActivator;
-import org.jboss.tools.usage.reporting.ReportingMessages;
-import org.jboss.tools.usage.util.StatusUtils;
-import org.osgi.service.prefs.BackingStoreException;
 
 /**
  * @author Andre Dieitsheim
@@ -29,32 +22,7 @@ public class UsageReportDispatcher implements IStartup {
 		Display.getDefault().asyncExec(new Runnable() {
 
 			public void run() {
-				UsageReportEnablementDialog dialog = new UsageReportEnablementDialog(ReportingMessages.UsageReport_DialogTitle,
-						ReportingMessages.UsageReport_DialogMessage,
-						ReportingMessages.UsageReport_Checkbox_Text,
-						true,
-						PlatformUI.getWorkbench().getActiveWorkbenchWindow());
-				if (UsageReportPreferences.isAskUser()) {
-					if (dialog.open() == Window.OK) {
-						UsageReportPreferences.setEnabled(dialog.isReportEnabled());
-						UsageReportPreferences.setAskUser(false);
-						flushPreferences();
-					}
-				}
-
-				if (UsageReportPreferences.isEnabled()) {
-					new UsageReport().report();
-				}
-			}
-
-			private void flushPreferences() {
-				try {
-					UsageReportPreferences.flush();
-				} catch (BackingStoreException e) {
-					IStatus status = StatusUtils.getErrorStatus(JBossToolsUsageActivator.PLUGIN_ID,
-							ReportingMessages.UsageReport_Error_SavePreferences, e);
-					JBossToolsUsageActivator.getDefault().getLog().log(status);
-				}
+				new UsageReport().report();
 			}
 		});
 	}
