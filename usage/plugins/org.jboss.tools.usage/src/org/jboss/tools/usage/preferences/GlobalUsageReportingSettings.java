e65fd9dd7e5f86525e844f3b13d28ed9b3df0a39
Andre Dietisheim <adietish@redhat.com>
2010-08-27 12:17:16 +0000

    [JBIDE-6880] defaults implemented (in case page to reachable)
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@24494 a97e2381-89e5-4abb-bab3-167db6db766c

--- usage/plugins/org.jboss.tools.usage/src/org/jboss/tools/usage/preferences/GlobalUsageReportingSettings.java
+++ usage/plugins/org.jboss.tools.usage/src/org/jboss/tools/usage/preferences/GlobalUsageReportingSettings.java
@@ -11,7 +11,6 @@
 package org.jboss.tools.usage.preferences;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.util.Map;
 
 import org.eclipse.core.runtime.Plugin;
@@ -24,45 +23,60 @@ import org.jboss.tools.usage.HttpResourceMap;
  */
 public class GlobalUsageReportingSettings extends HttpResourceMap {
 
+	private static final String REPORT_ENABLEMENT_ENABLEDVALUE = "ENABLED"; //$NON-NLS-1$
+
 	private static final String REPORTING_ENABLEMENT_URL = "http://community.jboss.org/wiki/JBossToolsJBossDeveloperStudioUsageReportingEnablement"; //$NON-NLS-1$
 
-	public static final String KEY_REPORT_ENABLEMENT = "Usage Reporting is ";
-	public static final String KEY_DUMMY_VALUE = "Dummy Value is ";
-	public static final String KEY_INTEGER_VALUE = "Integer Value is ";
+	public static final String REPORT_ENABLEMENT_KEY = "Usage Reporting is "; //$NON-NLS-1$
+	private static final boolean REPORT_ENABLEMENT_DEFAULT = true;
 
+	public static final String DUMMY_VALUE_KEY = "Dummy Value is "; //$NON-NLS-1$
+	public static final String INTEGER_VALUE_KEY = "Integer Value is "; //$NON-NLS-1$
+
+	/* the delimiter that delimits the value */
 	private static final char VALUE_DELIMITER = '<';
-
 
 	public GlobalUsageReportingSettings(Plugin plugin) throws IOException {
 		super(REPORTING_ENABLEMENT_URL
 				, VALUE_DELIMITER
 				, plugin
-				, KEY_REPORT_ENABLEMENT
-				, KEY_DUMMY_VALUE
-				, KEY_INTEGER_VALUE);
+				, REPORT_ENABLEMENT_KEY
+				, DUMMY_VALUE_KEY
+				, INTEGER_VALUE_KEY);
 
 		this.plugin = plugin;
 		this.urlConnection = createURLConnection(REPORTING_ENABLEMENT_URL);
 	}
 
-	public boolean isEnabled() throws UnsupportedEncodingException, IOException {
-		Map<String, String> valueMap = getValueMap();
-		String isEnabled = valueMap.get(KEY_REPORT_ENABLEMENT);
-		return isEnabled != null && "ENABLED".equals(isEnabled.toUpperCase());
-	}
-
-	public String getStringValue() throws UnsupportedEncodingException, IOException {
-		Map<String, String> valueMap = getValueMap();
-		return valueMap.get(KEY_DUMMY_VALUE);
-	}
-
-	public Integer getIntegerValue() throws UnsupportedEncodingException, IOException {
-		Map<String, String> valueMap = getValueMap();
-		String integerValue = valueMap.get(KEY_INTEGER_VALUE);
-		if(integerValue != null) {
-			return Integer.parseInt(integerValue);
-		} else {
-			return null;
+	public boolean isEnabled() {
+		try {
+			Map<String, String> valueMap = getValueMap();
+			String isEnabled = valueMap.get(REPORT_ENABLEMENT_KEY);
+			return isEnabled != null && REPORT_ENABLEMENT_ENABLEDVALUE.equals(isEnabled.toUpperCase());
+		} catch (Exception e) {
+			return REPORT_ENABLEMENT_DEFAULT;
 		}
+	}
+
+	public String getStringValue() {
+		try {
+			Map<String, String> valueMap = getValueMap();
+			return valueMap.get(DUMMY_VALUE_KEY);
+		} catch (Exception e) {
+			return "";
+		}
+	}
+
+	public Integer getIntegerValue() {
+		Integer returnValue = null;
+		try {
+			Map<String, String> valueMap = getValueMap();
+			String integerValue = valueMap.get(INTEGER_VALUE_KEY);
+			if (integerValue != null) {
+				returnValue = Integer.parseInt(integerValue);
+			}
+		} catch (Exception e) {
+		}
+		return returnValue;
 	}
 }
