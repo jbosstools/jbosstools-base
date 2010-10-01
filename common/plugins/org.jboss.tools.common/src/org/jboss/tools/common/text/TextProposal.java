5c14cf3e648a12d9cc4ee5da7ef19e0c18baca51
Victor Rubezhny <ruby@exadel.com>
2010-10-01 15:17:20 +0000

    JBIDE-7169 EL proposals should not be in the beginning of proposal lists in XML editor.
    Issue is fixed
    
    
    
    
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@25399 a97e2381-89e5-4abb-bab3-167db6db766c

--- common/plugins/org.jboss.tools.common/src/org/jboss/tools/common/text/TextProposal.java
+++ common/plugins/org.jboss.tools.common/src/org/jboss/tools/common/text/TextProposal.java
@@ -31,6 +31,7 @@ public class TextProposal {
 	public static final int R_XML_ATTRIBUTE_NAME = 910;
 	public static final int R_TAG_INSERTION = 1210;				// This value was changed from 500 to 1210 because it seems that according WTP's value was risen up to 1200 in 3.6 
 	public static final int R_XML_ATTRIBUTE_VALUE_TEMPLATE = 91;
+	public static final int R_XML_TAG_INSERTION = 91;
 	public static final int R_CLOSE_TAG = 1550;
 	
 	private static final long serialVersionUID = 3257007635692926512L;
