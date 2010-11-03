3ebc7622e0af79c6cede7657db76ad400ba386e3
Denis Golovin <dgolovin@exadel.com>
2010-11-03 23:03:45 +0000

    CDI PreferencePage tests
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@26239 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ tests/plugins/org.jboss.tools.tests/src/org/jboss/tools/test/util/PreferencePageAbstractTest.java
@@ -0,0 +1,33 @@
+package org.jboss.tools.test.util;
+
+import org.eclipse.jface.preference.PreferenceDialog;
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.dialogs.PreferencesUtil;
+import junit.framework.TestCase;
+
+public class PreferencePageAbstractTest extends TestCase {
+
+	public boolean isPreferencePageIsCreated(String id, Class expectedInstance) {
+		PreferenceDialog prefDialog = createPreferenceDialog(id);
+		
+		try {
+			prefDialog.setBlockOnOpen(false);
+			prefDialog.open();
+			
+			Object selectedPage = prefDialog.getSelectedPage();
+			return expectedInstance.isInstance(selectedPage); //$NON-NLS-1$
+		} finally {
+			prefDialog.close();
+		}
+		
+	}
+	
+	public static PreferenceDialog createPreferenceDialog(String pageId) {
+		PreferenceDialog dialog = PreferencesUtil.createPreferenceDialogOn(
+				 PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), pageId, new String[] {pageId}, null);
+		dialog.setBlockOnOpen(false);
+		return dialog;
+	}
+	
+}
