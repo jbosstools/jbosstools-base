da044bcedf06c4efe6d07a85954a47781b1d79e9
Alexey Kazakov <akazakov@exadel.com>
2011-03-03 20:37:19 +0000

    https://issues.jboss.org/browse/JBIDE-8281
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@29469 a97e2381-89e5-4abb-bab3-167db6db766c

--- common/plugins/org.jboss.tools.common.el.core/src/org/jboss/tools/common/el/core/resolver/ELResolverFactoryManager.java
+++ common/plugins/org.jboss.tools.common.el.core/src/org/jboss/tools/common/el/core/resolver/ELResolverFactoryManager.java
@@ -10,6 +10,7 @@
  ******************************************************************************/ 
 package org.jboss.tools.common.el.core.resolver;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -32,6 +33,9 @@ public class ELResolverFactoryManager {
 
 	private static final ELResolverFactoryManager INSTANCE = new ELResolverFactoryManager();
 
+	private static HashMap<String, Set<ELResolver>> resolversByNature;
+	private static Set<ELResolverFactory> resolverFactories;
+
 	private ELResolverFactoryManager() {
 	}
 
@@ -41,6 +45,55 @@ public class ELResolverFactoryManager {
 	 */
 	public static ELResolverFactoryManager getInstance() {
 		return INSTANCE;
+	}
+
+	private void init() {
+		if(resolversByNature==null) {
+			resolversByNature = new HashMap<String, Set<ELResolver>>();
+			resolverFactories = new HashSet<ELResolverFactory>();
+
+			IExtensionRegistry registry = Platform.getExtensionRegistry();
+			IExtensionPoint extensionPoint = registry.getExtensionPoint("org.jboss.tools.common.el.core.elResolver");  //$NON-NLS-1$
+			IExtension[] extensions = extensionPoint.getExtensions();
+			for (int i=0; i<extensions.length; i++) {
+				IExtension extension = extensions[i];
+				IConfigurationElement[] elements = extension.getConfigurationElements();
+				for(int j=0; j<elements.length; j++) {
+					IConfigurationElement[] natures = elements[j].getChildren("project-nature"); //$NON-NLS-1$
+					for (int k = 0; k < natures.length; k++) {
+						String natureId = natures[k].getAttribute("id"); //$NON-NLS-1$
+						try {
+							Object resolver = natures[k].createExecutableExtension("resolver-class"); //$NON-NLS-1$
+							if(resolver instanceof ELResolver) {
+								Set<ELResolver> resolverSet = resolversByNature.get(natureId);
+								if(resolverSet==null) {
+									resolverSet = new HashSet<ELResolver>();
+									resolversByNature.put(natureId, resolverSet);
+								}
+								resolverSet.add((ELResolver)resolver);
+							} else {
+								ELCorePlugin.getPluginLog().logError(resolver.getClass().getName() + " must be instance of org.jboss.tools.common.el.core.resolver.ELResolver"); //$NON-NLS-1$
+							}
+						} catch (CoreException e) {
+							ELCorePlugin.getPluginLog().logError(e);
+						}
+					}
+					IConfigurationElement[] factories = elements[j].getChildren("factory"); //$NON-NLS-1$
+					for (int k = 0; k < factories.length; k++) {
+						try {
+							Object factory = factories[k].createExecutableExtension("class"); //$NON-NLS-1$
+							if(factory instanceof ELResolverFactory) {
+								resolverFactories.add((ELResolverFactory)factory);
+							} else if(!(factory instanceof TestELResolverFactory)) {
+								ELCorePlugin.getPluginLog().logError(factory.getClass().getName() + " must be instance of org.jboss.tools.common.el.core.resolver.ELResolverFactory"); //$NON-NLS-1$
+							}
+						} catch (CoreException e) {
+							ELCorePlugin.getPluginLog().logError(e);
+						}
+					}
+				}
+			}
+		}
 	}
 
 	/**
@@ -53,49 +106,27 @@ public class ELResolverFactoryManager {
 			return new ELResolver[0];
 		}
 		IProject project = resource.getProject();
-		if (project == null || !project.isAccessible())
+		if (project == null || !project.isAccessible()) {
 			return new ELResolver[0];
+		}
+
+		init();
 
 		Set<ELResolver> resolverSet = new HashSet<ELResolver>();
-		IExtensionRegistry registry = Platform.getExtensionRegistry();
-		IExtensionPoint extensionPoint = registry.getExtensionPoint("org.jboss.tools.common.el.core.elResolver");  //$NON-NLS-1$
-		IExtension[] extensions = extensionPoint.getExtensions();
-		for (int i=0; i<extensions.length; i++) {
-			IExtension extension = extensions[i];
-			IConfigurationElement[] elements = extension.getConfigurationElements();
-			for(int j=0; j<elements.length; j++) {
-				IConfigurationElement[] natures = elements[j].getChildren("project-nature"); //$NON-NLS-1$
-				for (int k = 0; k < natures.length; k++) {
-					String natureId = natures[k].getAttribute("id"); //$NON-NLS-1$
-					try {
-						if(project.hasNature(natureId)) {
-							Object resolver = natures[k].createExecutableExtension("resolver-class"); //$NON-NLS-1$
-							if(resolver instanceof ELResolver) {
-								resolverSet.add((ELResolver)resolver);
-							} else {
-								ELCorePlugin.getPluginLog().logError(resolver.getClass().getName() + " must be instance of org.jboss.tools.common.el.core.resolver.ELResolver"); //$NON-NLS-1$
-							}
-						}
-					} catch (CoreException e) {
-						ELCorePlugin.getPluginLog().logError(e);
-					}
+		Set<String> ids = resolversByNature.keySet();
+		for (String natureId : ids) {
+			try {
+				if(project.hasNature(natureId)) {
+					resolverSet.addAll(resolversByNature.get(natureId));
 				}
-				IConfigurationElement[] factories = elements[j].getChildren("factory"); //$NON-NLS-1$
-				for (int k = 0; k < factories.length; k++) {
-					try {
-						Object factory = factories[k].createExecutableExtension("class"); //$NON-NLS-1$
-						if(factory instanceof ELResolverFactory) {
-							ELResolver resolver = ((ELResolverFactory)factory).createResolver(resource);
-							if(resolver!=null) {
-								resolverSet.add(resolver);
-							}
-						} else if(!(factory instanceof TestELResolverFactory)) {
-							ELCorePlugin.getPluginLog().logError(factory.getClass().getName() + " must be instance of org.jboss.tools.common.el.core.resolver.ELResolverFactory"); //$NON-NLS-1$
-						}
-					} catch (CoreException e) {
-						ELCorePlugin.getPluginLog().logError(e);
-					}
-				}
+			} catch (CoreException e) {
+				ELCorePlugin.getPluginLog().logError(e);
+			}
+		}
+		for (ELResolverFactory factory : resolverFactories) {
+			ELResolver resolver = factory.createResolver(resource);
+			if(resolver!=null) {
+				resolverSet.add(resolver);
 			}
 		}
 		return resolverSet.toArray(new ELResolver[resolverSet.size()]);
