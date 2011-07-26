a9a289892b74906def29bcb5635212a2dc7c90d8
Viacheslav Kabanovich <scabanovich@exadel.com>
2011-07-26 17:23:26 +0000

    JBIDE-9087 https://issues.jboss.org/browse/JBIDE-9087
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@33203 a97e2381-89e5-4abb-bab3-167db6db766c

--- common/plugins/org.jboss.tools.common.el.core/src/org/jboss/tools/common/el/core/resolver/MessagePropertyELSegmentImpl.java
+++ common/plugins/org.jboss.tools.common.el.core/src/org/jboss/tools/common/el/core/resolver/MessagePropertyELSegmentImpl.java
@@ -10,9 +10,15 @@
  ******************************************************************************/ 
 package org.jboss.tools.common.el.core.resolver;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.swt.graphics.Image;
 import org.jboss.tools.common.el.core.parser.LexicalToken;
+import org.jboss.tools.common.model.XModelObject;
+import org.jboss.tools.common.model.util.FindObjectHelper;
 import org.jboss.tools.common.text.ITextSourceReference;
 
 /**
@@ -26,6 +32,8 @@ public class MessagePropertyELSegmentImpl extends ELSegmentImpl implements
 	private int propertyStart=0, propertyLength=0;
 	private String baseName=null;
 	private boolean isBundle = false;
+
+	private List<XModelObject> objects = new ArrayList<XModelObject>();
 
 	public MessagePropertyELSegmentImpl(LexicalToken token) {
 		super(token);
@@ -80,5 +88,40 @@ public class MessagePropertyELSegmentImpl extends ELSegmentImpl implements
 	
 	public void setBaseName(String name){
 		baseName = name;
+	}
+
+	public void addObject(XModelObject object) {
+		objects.add(object);
+	}
+
+	public IOpenableReference[] getOpenable() {
+		if(objects != null) {
+			IOpenableReference[] result = new IOpenableReference[objects.size()];
+			for (int i = 0; i < objects.size(); i++) {
+				final XModelObject o = objects.get(i);
+				result[i] = new IOpenableReference() {
+					@Override
+					public boolean open() {
+						int q = FindObjectHelper.findModelObject(o, FindObjectHelper.IN_EDITOR_ONLY);
+						return  q < 1;
+					}
+					
+					@Override
+					public String getLabel() {
+						XModelObject p = o;
+						while(p != null && p.getFileType() < XModelObject.FILE) p = p.getParent();
+						//improve label - now it returns file name, with locale
+						return p.getAttributeValue("name");
+					}
+					
+					@Override
+					public Image getImage() {
+						return null;
+					}
+				};
+			}
+			return result;
+		}
+		return super.getOpenable();
 	}
 }
