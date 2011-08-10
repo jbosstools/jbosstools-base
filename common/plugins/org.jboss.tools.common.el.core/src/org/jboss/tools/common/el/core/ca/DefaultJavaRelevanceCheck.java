983de81c794f96848034cb04168d6ec72a1c930d
Daniel Azarov <daniel@exadel.com>
2011-08-10 00:42:25 +0000

    https://issues.jboss.org/browse/JBIDE-9473
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@33745 a97e2381-89e5-4abb-bab3-167db6db766c

--- common/plugins/org.jboss.tools.common.el.core/src/org/jboss/tools/common/el/core/ca/DefaultJavaRelevanceCheck.java
+++ common/plugins/org.jboss.tools.common.el.core/src/org/jboss/tools/common/el/core/ca/DefaultJavaRelevanceCheck.java
@@ -12,12 +12,14 @@ package org.jboss.tools.common.el.core.ca;
 
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.IType;
 import org.jboss.tools.common.el.core.resolver.IRelevanceCheck;
 
 public class DefaultJavaRelevanceCheck implements IRelevanceCheck {
 	String test1 = null;
 	String test2 = null;
 	String test3 = null;
+	boolean isIType = false;
 
 	public DefaultJavaRelevanceCheck(IJavaElement element) {
 		if(element == null) return;
@@ -31,10 +33,15 @@ public class DefaultJavaRelevanceCheck implements IRelevanceCheck {
 				test3 = test1.substring(2);
 			}
 			if(test3 != null && test3.equals(test2)) test3 = null;
+		}else if(element instanceof IType){
+			isIType = true;
 		}
 	}
 	
 	public boolean isRelevant(String content) {
+		if(isIType)
+			return true;
+		
 		if(test1 != null && content.contains(test1)) return true;
 		if(test2 != null && content.contains(test2)) return true;
 		if(test3 != null && content.contains(test3)) return true;
