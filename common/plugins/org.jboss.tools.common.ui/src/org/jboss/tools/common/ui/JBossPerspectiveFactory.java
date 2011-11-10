9da3e6e4b59ea37fc00113b67701dbdbbdfc1cad
Alexey Kazakov <akazakov@exadel.com>
2011-11-10 00:17:15 +0000

    https://issues.jboss.org/browse/JBIDE-10140 JBoss Perspective
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@36255 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ common/plugins/org.jboss.tools.common.ui/src/org/jboss/tools/common/ui/JBossPerspectiveFactory.java
@@ -0,0 +1,85 @@
+/******************************************************************************* 
+ * Copyright (c) 2011 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/
+package org.jboss.tools.common.ui;
+
+import org.eclipse.debug.ui.IDebugUIConstants;
+import org.eclipse.jdt.ui.JavaUI;
+import org.eclipse.ui.IFolderLayout;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipse.ui.navigator.resources.ProjectExplorer;
+import org.eclipse.ui.progress.IProgressConstants;
+
+/**
+ * @author Alexey Kazakov
+ */
+public class JBossPerspectiveFactory implements IPerspectiveFactory {
+
+	public static final String PERSPECTIVE_ID = "org.jboss.tools.common.ui.JBossPerspective"; //$NON-NLS-1$
+
+	protected static final String ID_SERVERS_VIEW = "org.eclipse.wst.server.ui.ServersView"; //$NON-NLS-1$
+	protected static final String ID_SEARCH_VIEW = "org.eclipse.search.ui.views.SearchView"; //$NON-NLS-1$
+	protected static final String ID_CONSOLE_VIEW = "org.eclipse.ui.console.ConsoleView"; //$NON-NLS-1$
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IPerspectiveFactory#createInitialLayout(org.eclipse.ui.IPageLayout)
+	 */
+	@Override
+	public void createInitialLayout(IPageLayout layout) {
+		layout.addActionSet("org.eclipse.jst.j2ee.J2eeMainActionSet"); //$NON-NLS-1$
+		layout.addActionSet(JavaUI.ID_ACTION_SET);
+
+		layout.addActionSet(IDebugUIConstants.LAUNCH_ACTION_SET);
+		layout.addActionSet(IDebugUIConstants.DEBUG_ACTION_SET);
+
+		layout.addActionSet(IPageLayout.ID_NAVIGATE_ACTION_SET);
+
+		layout.addShowViewShortcut(ProjectExplorer.VIEW_ID);
+		layout.addShowViewShortcut(ID_SERVERS_VIEW);
+		layout.addShowViewShortcut(IPageLayout.ID_BOOKMARKS);
+		layout.addShowViewShortcut(IPageLayout.ID_OUTLINE);
+		layout.addShowViewShortcut(IPageLayout.ID_PROP_SHEET);
+		layout.addShowViewShortcut(IPageLayout.ID_RES_NAV);
+		layout.addShowViewShortcut(IPageLayout.ID_PROBLEM_VIEW);
+		
+		// views - search
+		layout.addShowViewShortcut(ID_SEARCH_VIEW);
+		// views - debugging
+		layout.addShowViewShortcut(ID_CONSOLE_VIEW);
+
+		layout.addShowInPart(ProjectExplorer.VIEW_ID);
+
+		String editorArea = layout.getEditorArea();
+
+		// Top left.
+		IFolderLayout topLeft = layout.createFolder("topLeft", IPageLayout.LEFT, 0.25f, editorArea);//$NON-NLS-1$
+		topLeft.addView(ProjectExplorer.VIEW_ID);
+		topLeft.addPlaceholder(IPageLayout.ID_RES_NAV);
+		topLeft.addPlaceholder(JavaUI.ID_TYPE_HIERARCHY);
+		topLeft.addPlaceholder(JavaUI.ID_PACKAGES_VIEW);
+
+		// Bottom right.
+		IFolderLayout bottomRight = layout.createFolder("bottomRight", IPageLayout.BOTTOM, 0.7f, editorArea);//$NON-NLS-1$
+		bottomRight.addView(IPageLayout.ID_PROBLEM_VIEW);
+		bottomRight.addView(IPageLayout.ID_PROP_SHEET);
+		bottomRight.addView(ID_SERVERS_VIEW);
+
+		bottomRight.addPlaceholder(IPageLayout.ID_TASK_LIST);
+		bottomRight.addPlaceholder(ID_CONSOLE_VIEW);
+		bottomRight.addPlaceholder(IPageLayout.ID_BOOKMARKS);
+		bottomRight.addPlaceholder(IProgressConstants.PROGRESS_VIEW_ID);
+		bottomRight.addPlaceholder(ID_SEARCH_VIEW);
+
+		// Top right.
+		IFolderLayout topRight = layout.createFolder("topRight", IPageLayout.RIGHT, 0.7f, editorArea);//$NON-NLS-1$
+		topRight.addView(IPageLayout.ID_OUTLINE);
+	}
+}
