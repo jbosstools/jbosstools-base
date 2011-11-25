232cad81a6174093333a38d6a3b3bbf21272ed8b
Yahor Radtsevich <yradtsevich@exadel.com>
2011-11-25 14:42:23 +0000

    https://issues.jboss.org/browse/JBIDE-10221 / https://issues.jboss.org/browse/JBIDE-10249 : org.jboss.tools.vpe plugin has Image leak
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@36661 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ common/plugins/org.jboss.tools.common/src/org/jboss/tools/common/util/SwtUtil.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2007-2011 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.common.util;
+
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.graphics.Resource;
+import org.eclipse.swt.widgets.Widget;
+
+/**
+ * @author Yahor Radtsevich (yradtsevich)
+ */
+public class SwtUtil {
+	
+	/**
+	 * Adds a dispose listener to {@code widget}
+	 * which when invoked disposes given {@code resource}
+	 * 
+	 * @param resource resource to be disposed
+	 * @param widget widget to which disposal bind disposal of {@code resource}
+	 */
+	public static void bindDisposal(final Resource resource, Widget widget) {
+		widget.addDisposeListener(new DisposeListener() {
+			public void widgetDisposed(DisposeEvent e) {
+				resource.dispose();
+				System.out.println("Resource Disposed: " + resource);
+			}
+		});
+	}
+}
