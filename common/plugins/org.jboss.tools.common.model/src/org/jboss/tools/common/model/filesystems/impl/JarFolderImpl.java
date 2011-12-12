af2d03703e2dde9c0cb515fded613ed5d6f48ab0
Viacheslav Kabanovich <scabanovich@exadel.com>
2011-12-12 22:55:46 +0000

    JBIDE-10442 https://issues.jboss.org/browse/JBIDE-10442 Do not read content of .class entries.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@37246 a97e2381-89e5-4abb-bab3-167db6db766c

--- common/plugins/org.jboss.tools.common.model/src/org/jboss/tools/common/model/filesystems/impl/JarFolderImpl.java
+++ common/plugins/org.jboss.tools.common.model/src/org/jboss/tools/common/model/filesystems/impl/JarFolderImpl.java
@@ -93,7 +93,7 @@ public class JarFolderImpl extends RegularObjectImpl implements FolderLoader {
         if("FileAny".equals(entity)) { //$NON-NLS-1$
             if(jar.getSize(cpath) > 100000) entity = XModelObjectConstants.ENT_FILE_ANY_LONG;
             else if(jar.isTextEntry(cpath, 100)) entity = "FileTXT"; //$NON-NLS-1$
-        } else /*if(entity == null)*/ {
+        } else if(!"FileCLASS".equals(entity)) { //$NON-NLS-1$
             body = jar.getContent(cpath);
             entity = getModel().getEntityRecognizer().getEntityName(new EntityRecognizerContext(name, ext, body));
         }
