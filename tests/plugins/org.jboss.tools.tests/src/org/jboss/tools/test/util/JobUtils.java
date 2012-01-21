a3294e3a2e340a5f270725eaac18dc113a072b1a
Denis Golovin <dgolovin@exadel.com>
2012-01-21 01:54:22 +0000

    JBIDE-8997 CLONE - 2010 in the copyright headers should be replaced by 2011 fixed copyright comment and date for tests component
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@38014 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/plugins/org.jboss.tools.tests/src/org/jboss/tools/test/util/JobUtils.java
+++ tests/plugins/org.jboss.tools.tests/src/org/jboss/tools/test/util/JobUtils.java
@@ -1,14 +1,13 @@
 /******************************************************************************* 
- * Copyright (c) 2007 Red Hat, Inc. 
+ * Copyright (c) 2007-2012 Red Hat, Inc. 
  * Distributed under license by Red Hat, Inc. All rights reserved. 
  * This program is made available under the terms of the 
  * Eclipse Public License v1.0 which accompanies this distribution, 
  * and is available at http://www.eclipse.org/legal/epl-v10.html 
  * 
  * Contributors: 
- * Red Hat, Inc. - initial API and implementation 
+ *     Red Hat, Inc. - initial API and implementation 
  ******************************************************************************/
-
 package org.jboss.tools.test.util;
 
 import org.eclipse.core.runtime.jobs.Job;
--- tests/plugins/org.jboss.tools.tests/src/org/jboss/tools/test/util/XMLEntityResolver.java
+++ tests/plugins/org.jboss.tools.tests/src/org/jboss/tools/test/util/XMLEntityResolver.java
@@ -1,8 +1,13 @@
-/*
- * XMLEntityResolver.java
- * Created on February 21, 2003, 9:38 AM
- */
-
+/******************************************************************************* 
+ * Copyright (c) 2007-2012 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ *     Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/
 package org.jboss.tools.test.util;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
