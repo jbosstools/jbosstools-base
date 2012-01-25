9bb9d13419c92e1e3922462ffe09e35f6609ff2c
Viacheslav Kabanovich <scabanovich@exadel.com>
2012-01-25 18:50:10 +0000

    JBIDE-10703 https://issues.jboss.org/browse/JBIDE-10703 resref.ui.Activator is modified to follow the common pattern.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@38186 a97e2381-89e5-4abb-bab3-167db6db766c

--- common/plugins/org.jboss.tools.common.resref.ui/src/org/jboss/tools/common/resref/ui/Activator.java
+++ common/plugins/org.jboss.tools.common.resref.ui/src/org/jboss/tools/common/resref/ui/Activator.java
@@ -11,7 +11,6 @@
 package org.jboss.tools.common.resref.ui;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
-import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -28,24 +27,7 @@ public class Activator extends AbstractUIPlugin {
 	 * The constructor
 	 */
 	public Activator() {
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
 		plugin = this;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
 	}
 
 	/**
