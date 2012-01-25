de5138d37b4969ac57c1667e636b7ecc6d063eff
Viacheslav Kabanovich <scabanovich@exadel.com>
2012-01-25 18:36:53 +0000

    Obsolete code is removed.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@38184 a97e2381-89e5-4abb-bab3-167db6db766c

--- common/plugins/org.jboss.tools.common.model.ui/src/org/jboss/tools/common/model/ui/ModelUIPlugin.java
+++ common/plugins/org.jboss.tools.common.model.ui/src/org/jboss/tools/common/model/ui/ModelUIPlugin.java
@@ -10,70 +10,37 @@
  ******************************************************************************/ 
 package org.jboss.tools.common.model.ui;
 
-import java.util.MissingResourceException;
-import java.util.ResourceBundle;
-
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.jboss.tools.common.log.BaseUIPlugin;
 import org.jboss.tools.common.log.IPluginLog;
 import org.jboss.tools.common.model.plugin.IModelPlugin;
-import org.osgi.framework.BundleContext;
 
 public class ModelUIPlugin extends BaseUIPlugin implements IModelPlugin {
 	public static final String PLUGIN_ID = "org.jboss.tools.common.model.ui";  //$NON-NLS-1$
-	private static ModelUIPlugin INSTANCE;
-	private ResourceBundle resourceBundle;
+	private static ModelUIPlugin plugin;
 
 	public ModelUIPlugin() {
-		INSTANCE = this;
-		try {
-			resourceBundle= ResourceBundle.getBundle(PLUGIN_ID + ".PluginResources"); //$NON-NLS-1$
-		} 
-		catch (MissingResourceException x) {
-			resourceBundle = null;
-		}
+		plugin = this;
 	}
 
 	public static IWorkspace getWorkspace() {
 		return ResourcesPlugin.getWorkspace();
 	}
 
-	public static String getResourceString(String key) {
-		ResourceBundle bundle= ModelUIPlugin.getDefault().getResourceBundle();
-		try	{
-			return bundle.getString(key);
-		} catch (MissingResourceException e) {
-			return key;
-		}
-	}
-
-	public ResourceBundle getResourceBundle() {
-		return resourceBundle;
-	}
-	
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-	}
-
 	public static ModelUIPlugin getDefault() {
-		return INSTANCE;
+		return plugin;
 	}
 
 	public static boolean isDebugEnabled() {
-		return INSTANCE.isDebugging();
-	}
-	
+		return plugin.isDebugging();
+	}	
 
     public static final String PROJECT_OVERRIDE = "org.jboss.tools.common.model.ui.templates.projectOverriveTemplates"; //$NON-NLS-1$
 //    public static final String PROJECT_CAN_OVERRIDE = "org.jboss.tools.common.model.ui.templates.projectCanOverriveTemplates";
 	public static final String ID_PLUGIN= "org.jboss.tools.common.model.ui.templates"; //$NON-NLS-1$
 	public static final String META_TEMPLATES_DTD = "-//Red Hat Inc.//DTD Meta Templates 1.0//EN"; //$NON-NLS-1$
 
-	public static String getPluginId() {
-		return ModelUIPlugin.ID_PLUGIN;
-	}
-	
 	/**
 	 * @return IPluginLog object
 	 */
