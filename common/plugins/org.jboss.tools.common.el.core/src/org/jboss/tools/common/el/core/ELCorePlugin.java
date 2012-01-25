1323d796d7bdafdfcdcc082e4e40d2baf0d5648c
Viacheslav Kabanovich <scabanovich@exadel.com>
2012-01-25 18:16:40 +0000

    JBIDE-10703 https://issues.jboss.org/browse/JBIDE-10703 ELCorePlugin is modified to follow the common pattern.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@38182 a97e2381-89e5-4abb-bab3-167db6db766c

--- common/plugins/org.jboss.tools.common.el.core/src/org/jboss/tools/common/el/core/ELCorePlugin.java
+++ common/plugins/org.jboss.tools.common.el.core/src/org/jboss/tools/common/el/core/ELCorePlugin.java
@@ -12,7 +12,6 @@ package org.jboss.tools.common.el.core;
 
 import org.jboss.tools.common.log.BaseUIPlugin;
 import org.jboss.tools.common.log.IPluginLog;
-import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -29,24 +28,7 @@ public class ELCorePlugin extends BaseUIPlugin {
 	 * The constructor
 	 */
 	public ELCorePlugin() {
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
 		plugin = this;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
 	}
 
 	/**
