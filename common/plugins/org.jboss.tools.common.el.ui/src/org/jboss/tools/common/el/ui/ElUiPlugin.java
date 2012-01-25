7443847e5dc30a609b372cb5ec8c916d0e8f2853
Viacheslav Kabanovich <scabanovich@exadel.com>
2012-01-25 18:26:35 +0000

    JBIDE-10703 https://issues.jboss.org/browse/JBIDE-10703 ElUiPlugin is modified to follow the common pattern.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@38183 a97e2381-89e5-4abb-bab3-167db6db766c

--- common/plugins/org.jboss.tools.common.el.ui/src/org/jboss/tools/common/el/ui/ElUiPlugin.java
+++ common/plugins/org.jboss.tools.common.el.ui/src/org/jboss/tools/common/el/ui/ElUiPlugin.java
@@ -12,7 +12,6 @@
 package org.jboss.tools.common.el.ui;
 
 import org.jboss.tools.common.log.BaseUIPlugin;
-import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -29,24 +28,7 @@ public class ElUiPlugin extends BaseUIPlugin {
 	 * The constructor
 	 */
 	public ElUiPlugin() {
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
 		plugin = this;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
 	}
 
 	/**
