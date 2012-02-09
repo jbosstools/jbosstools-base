02bc3929d3a9f47eac73b56b1e3b4118f00ab0ce
Lucia Jelinkova <ljelinko@redhat.com>
2012-02-09 15:47:09 +0000

    changed the normal duration
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@38562 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/plugins/org.jboss.tools.ui.bot.ext/src/org/jboss/tools/ui/bot/ext/condition/TaskDuration.java
+++ tests/plugins/org.jboss.tools.ui.bot.ext/src/org/jboss/tools/ui/bot/ext/condition/TaskDuration.java
@@ -1,7 +1,7 @@
 package org.jboss.tools.ui.bot.ext.condition;
 
 public enum TaskDuration {
-	SHORT(1 * 1000), NORMAL(10 * 1000), LONG(1 * 60 * 1000), VERY_LONG(10 * 60 * 1000);
+	SHORT(10 * 1000), NORMAL(30 * 1000), LONG(1 * 60 * 1000), VERY_LONG(10 * 60 * 1000);
 	
 	private long timeout;
 	
