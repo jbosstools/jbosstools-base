7038553f249ef2779510e411e148e3d3df294fbc
Jiri Peterka <peterka.jiri@gmail.com>
2012-03-06 15:05:16 +0000

    ErrorLog bot ext delete method added
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@39303 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/plugins/org.jboss.tools.ui.bot.ext/src/org/jboss/tools/ui/bot/ext/view/ErrorLogView.java
+++ tests/plugins/org.jboss.tools.ui.bot.ext/src/org/jboss/tools/ui/bot/ext/view/ErrorLogView.java
@@ -1,6 +1,8 @@
 package org.jboss.tools.ui.bot.ext.view;
 
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
+import org.eclipse.swtbot.swt.finder.waits.Conditions;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
 import org.jboss.tools.ui.bot.ext.gen.ActionItem;
 import org.jboss.tools.ui.bot.ext.helper.ContextMenuHelper;
@@ -13,6 +15,9 @@ import org.jboss.tools.ui.bot.ext.helper.ContextMenuHelper;
  */
 public class ErrorLogView extends ViewBase {
 
+	/**
+	 * logs all error log messages into a logger
+	 */
 	public void logMessages() {
 		SWTBotTreeItem[] items = getView().bot().tree().getAllItems();
 		for (SWTBotTreeItem i : items)
@@ -29,9 +34,27 @@ public class ErrorLogView extends ViewBase {
 		log.info(item.getText());
 	}
 
+	/**
+	 * clear error log (temporarily)
+	 */
 	public void clear() {
 		ContextMenuHelper.clickContextMenu(getView().bot().tree(),
 				"Clear Log Viewer");
+		
+		log.info("Error log cleared");
+	}
+	
+	/**
+	 * deletes error log permanently
+	 */
+	public void delete() {
+		ContextMenuHelper.clickContextMenu(getView().bot().tree(),
+				"Delete Log");
+		
+		SWTBotShell shell = bot.shell("Confirm Delete").activate();
+		shell.bot().button("OK").click();
+		bot.waitUntil(Conditions.shellCloses(shell));
+		log.info("Error log content deleted");		
 	}
 
 	private SWTBotView getView() {
@@ -39,6 +62,10 @@ public class ErrorLogView extends ViewBase {
 		return view;
 	}
 
+	/**
+	 * 
+	 * @return all item count in the error log tree
+	 */
 	public int getRecordCount() {
 		int count = getView().bot().tree().getAllItems().length;
 		return count;
