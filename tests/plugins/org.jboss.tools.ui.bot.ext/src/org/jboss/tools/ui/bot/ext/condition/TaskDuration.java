83960900731787daeca63a2f17099acbe4aae0a4
Lucia Jelinkova <ljelinko@redhat.com>
2012-04-02 08:41:23 +0000

    changed the task duration constants
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@39963 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/plugins/org.jboss.tools.ui.bot.ext/src/org/jboss/tools/ui/bot/ext/condition/TaskDuration.java
+++ tests/plugins/org.jboss.tools.ui.bot.ext/src/org/jboss/tools/ui/bot/ext/condition/TaskDuration.java
@@ -1,7 +1,7 @@
 package org.jboss.tools.ui.bot.ext.condition;
 
 public enum TaskDuration {
-	SHORT(10 * 1000), NORMAL(30 * 1000), LONG(1 * 60 * 1000), VERY_LONG(10 * 60 * 1000);
+	SHORT(10 * 1000), NORMAL(60 * 1000), LONG(5 * 60 * 1000), VERY_LONG(10 * 60 * 1000);
 	
 	private long timeout;
 	
