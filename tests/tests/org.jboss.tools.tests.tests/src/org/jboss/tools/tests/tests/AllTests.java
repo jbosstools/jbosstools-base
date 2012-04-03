d5f658aee159d30a41340fe94102800595763822
Mickael Istria <mickael.istria@gmail.com>
2012-04-03 09:41:52 +0000

    JBIDE-11370: Added tests for GWT & Tests.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@39993 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ tests/tests/org.jboss.tools.tests.tests/src/org/jboss/tools/tests/tests/AllTests.java
@@ -0,0 +1,13 @@
+package org.jboss.tools.tests.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	UtilsTest.class
+})
+public class AllTests {
+
+}
---
+++ tests/tests/org.jboss.tools.tests.tests/src/org/jboss/tools/tests/tests/UtilsTest.java
@@ -0,0 +1,14 @@
+package org.jboss.tools.tests.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UtilsTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
