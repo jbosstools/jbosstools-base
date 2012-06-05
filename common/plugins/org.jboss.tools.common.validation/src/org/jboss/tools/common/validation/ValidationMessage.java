408a1d8e75c7a8a43ede49b187849bad1450d041
Alexey Kazakov <akazakov@exadel.com>
2012-06-05 20:57:38 +0000

    https://issues.jboss.org/browse/JBIDE-10738 As-you-type EL validation
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@41727 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ common/plugins/org.jboss.tools.common.validation/src/org/jboss/tools/common/validation/ValidationMessage.java
@@ -0,0 +1,39 @@
+/******************************************************************************* 
+ * Copyright (c) 2012 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/
+package org.jboss.tools.common.validation;
+
+import java.util.Locale;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.wst.validation.internal.core.Message;
+import org.jboss.tools.common.CommonPlugin;
+
+/**
+ * @author Alexey Kazakov
+ */
+public class ValidationMessage extends Message {
+
+	private String message;
+
+	public ValidationMessage(int severity,	String message, IFile file) {
+		super(CommonPlugin.PLUGIN_ID, severity, message, null, file);
+		this.message = message;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.wst.validation.internal.core.Message#getText(java.util.Locale, java.lang.ClassLoader)
+	 */
+	@Override
+	public java.lang.String getText(Locale locale, ClassLoader classLoader) {
+		return message;
+	}
+}
