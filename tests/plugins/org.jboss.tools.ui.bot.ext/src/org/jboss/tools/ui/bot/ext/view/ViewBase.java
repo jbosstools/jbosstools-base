06ba6f9968bb405f55e3413e179a38ec905cd889
Lucia Jelinkova <ljelinko@redhat.com>
2012-06-07 13:45:32 +0000

    Added method getToolbarButtons to fix Juno issues with SWTBot (fix taken from Eclipse Bug 375598 proposed patch)
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@41770 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/plugins/org.jboss.tools.ui.bot.ext/src/org/jboss/tools/ui/bot/ext/view/ViewBase.java
+++ tests/plugins/org.jboss.tools.ui.bot.ext/src/org/jboss/tools/ui/bot/ext/view/ViewBase.java
@@ -1,8 +1,29 @@
 package org.jboss.tools.ui.bot.ext.view;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.log4j.Logger;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.ToolBarManager;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.ToolBar;
+import org.eclipse.swt.widgets.ToolItem;
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
 import org.eclipse.swtbot.swt.finder.SWTBot;
+import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
+import org.eclipse.swtbot.swt.finder.finders.UIThreadRunnable;
+import org.eclipse.swtbot.swt.finder.results.ListResult;
+import org.eclipse.swtbot.swt.finder.utils.SWTUtils;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotToolbarButton;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotToolbarDropDownButton;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotToolbarPushButton;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotToolbarRadioButton;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotToolbarSeparatorButton;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotToolbarToggleButton;
+import org.eclipse.ui.IViewSite;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.internal.WorkbenchPartReference;
 import org.jboss.tools.ui.bot.ext.SWTBotExt;
 import org.jboss.tools.ui.bot.ext.SWTOpenExt;
 import org.jboss.tools.ui.bot.ext.SWTTestExt;
@@ -45,4 +66,44 @@ public class ViewBase {
 		return open.viewOpen(viewObject);
 	}
 
+	public List<SWTBotToolbarButton> getToolbarButtons() {
+		return UIThreadRunnable.syncExec(new ListResult<SWTBotToolbarButton>() {
+
+			public List<SWTBotToolbarButton> run() {
+				SWTBotView view = show();
+				IWorkbenchPart obj = ((WorkbenchPartReference) view.getReference()).getPart(false);
+				ToolBar toolbar = null;
+				IToolBarManager t = ((IViewSite)obj.getSite()).getActionBars().getToolBarManager();
+				if (t instanceof ToolBarManager) {
+				    toolbar = ((ToolBarManager)t).getControl();
+				}
+
+				final List<SWTBotToolbarButton> l = new ArrayList<SWTBotToolbarButton>();
+
+				if (toolbar == null)
+					return l;
+
+				ToolItem[] items = toolbar.getItems();
+				for (int i = 0; i < items.length; i++) {
+					try {
+						if (SWTUtils.hasStyle(items[i], SWT.PUSH))
+							l.add(new SWTBotToolbarPushButton(items[i]));
+						else if(SWTUtils.hasStyle(items[i], SWT.CHECK))
+							l.add(new SWTBotToolbarToggleButton(items[i]));
+						else if(SWTUtils.hasStyle(items[i], SWT.RADIO))
+							l.add(new SWTBotToolbarRadioButton(items[i]));
+						else if(SWTUtils.hasStyle(items[i], SWT.DROP_DOWN))
+							l.add(new SWTBotToolbarDropDownButton(items[i]));
+						else if(SWTUtils.hasStyle(items[i], SWT.SEPARATOR))
+							l.add(new SWTBotToolbarSeparatorButton(items[i]));
+					} catch (WidgetNotFoundException e) {
+						e.printStackTrace();
+					}
+				}
+
+				return l;
+
+			}
+		});
+	}
 }
