551672dd8af637a7f56d89fa19401582cdcd21c8
Alexey Kazakov <akazakov@exadel.com>
2012-06-13 22:44:42 +0000

    https://issues.jboss.org/browse/JBIDE-10738 As-you-type EL validation
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@41947 a97e2381-89e5-4abb-bab3-167db6db766c

--- common/plugins/org.jboss.tools.common.validation/src/org/jboss/tools/common/validation/AsYouTypeValidatorManager.java
+++ common/plugins/org.jboss.tools.common.validation/src/org/jboss/tools/common/validation/AsYouTypeValidatorManager.java
@@ -158,12 +158,10 @@ public class AsYouTypeValidatorManager implements ISourceValidator, org.eclipse.
 			return;
 		}
 		for (IValidator validator : context.getValidators()) {
-			if(validator instanceof IAsYouTypeValidator) {
-				IProject rootProject = rootProjects.get(validator);
-				IValidatingProjectSet projectBrunch = context.getValidatingProjectTree(validator).getBrunches().get(rootProject);
-				if(projectBrunch!=null) {
-					((IAsYouTypeValidator)validator).validate(this, rootProject, dirtyRegion, helper, reporter, context, projectBrunch.getRootContext(), file);
-				}
+			IProject rootProject = rootProjects.get(validator);
+			IValidatingProjectSet projectBrunch = context.getValidatingProjectTree(validator).getBrunches().get(rootProject);
+			if(projectBrunch!=null) {
+				((IAsYouTypeValidator)validator).validate(this, rootProject, dirtyRegion, helper, reporter, context, projectBrunch.getRootContext(), file);
 			}
 		}
 //		reporter.removeAllMessages(AsYouTypeValidatorManager.this, file);
