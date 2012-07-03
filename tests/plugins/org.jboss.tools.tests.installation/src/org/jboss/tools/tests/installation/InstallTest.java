9d2a4374b273f9b9df10d258f7674631d3c3558c
Mickael Istria <mickael.istria@gmail.com>
2012-07-03 15:33:36 +0000

    JBIDE-11713: Moved scenario and script to tests/
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@42346 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ tests/plugins/org.jboss.tools.tests.installation/src/org/jboss/tools/tests/installation/InstallTest.java
@@ -0,0 +1,153 @@
+/******************************************************************************* 
+ * Copyright (c) 2012 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ *     Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/
+
+package org.jboss.tools.tests.installation;
+
+import junit.framework.Assert;
+
+import org.eclipse.swtbot.eclipse.finder.SWTBotEclipseTestCase;
+import org.eclipse.swtbot.swt.finder.SWTBot;
+import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
+import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtbot.swt.finder.waits.ICondition;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * This is a bot scenario which performs install through p2 UI.
+ * It takes as input a p2 repository URL configured in the UPDATE_SITE
+ * system property.
+ * 
+ * @author Mickael Istria
+ */
+@RunWith(SWTBotJunit4ClassRunner.class)
+public class InstallTest extends SWTBotEclipseTestCase {
+	
+	/**
+	 * System property expected to receive URL of a p2 repo to install
+	 * It the input of the scenario.
+	 */
+	public static final String UPDATE_SITE_PROPERTY = "UPDATE_SITE";
+
+	@Test
+	public void testInstall() throws Exception {
+		String site = System.getProperty("UPDATE_SITE");
+		Assert.assertNotNull("No site specified, set UPDATE_SITE system property first", site);
+		
+		try {
+			installFromSite(site);
+		} catch (Exception ex) {
+			String installDesc = this.bot.text().getText();
+			if (installDesc == null || installDesc.isEmpty()) {
+				throw new RuntimeException("Internal error", ex);
+			}
+			StringBuilder message = new StringBuilder();
+			message.append("Could not install from: " + site);
+			message.append(installDesc);
+			System.err.println(message.toString());
+			Assert.fail(message.toString());
+		}
+	}
+
+	private void installFromSite(String site) {
+		this.bot.menu("Help").menu("Install New Software...").click();
+		this.bot.shell("Install").bot().button("Add...").click();
+		this.bot.shell("Add Repository").activate().setFocus();
+		this.bot.text(1).setText(site);
+		this.bot.button("OK").click();
+		this.bot.shell("Install").activate().setFocus();
+		this.bot.waitWhile(new ICondition() {
+			
+			@Override
+			public boolean test() throws Exception {
+				return bot.tree().getAllItems()[0].getText().startsWith("Pending...");
+			}
+			
+			@Override
+			public void init(SWTBot bot) {
+			}
+			
+			@Override
+			public String getFailureMessage() {
+				return "Could not see categories in tree";
+			}
+		});
+		this.bot.button("Select All").click();
+		this.bot.button("Next >").click();
+		this.bot.waitUntil(new ICondition() {
+			@Override
+			public boolean test() throws Exception {
+				return bot.button("Cancel").isEnabled();
+			}
+			
+			@Override
+			public void init(SWTBot bot) {
+			}
+			
+			@Override
+			public String getFailureMessage() {
+				return "Blocking while calculating deps";
+			}
+		}, 10 * 60000); // 5 minutes timeout
+		this.bot.button("Next >").click();
+		this.bot.radio(0).click();
+		this.bot.button("Finish").click();
+		// wait for Security pop-up, or install finished.
+		final SWTBotShell shell = this.bot.shell("Installing Software");
+		this.bot.waitWhile(new ICondition() {
+			
+			@Override
+			public boolean test() throws Exception {
+				return shell.isActive();
+			}
+			
+			@Override
+			public void init(SWTBot bot) {
+			}
+			
+			@Override
+			public String getFailureMessage() {
+				return null;
+			}
+		}, 20 * 60000); // 20 minutes_tino
+		if (this.bot.activeShell().getText().equals("Security Warning")) {
+			this.bot.button("OK").click();
+			System.err.println("OK clicked");
+			this.bot.waitUntil(new ICondition() {
+				@Override
+				public boolean test() throws Exception {
+					try {
+						boolean stillOpen = bot.shell("Installing Software").isOpen();
+						System.err.println("still open? " + stillOpen);
+						return !stillOpen;
+					} catch (WidgetNotFoundException ex) {
+						System.err.println("no shell");
+						// Shell already closed
+						return true;
+					}
+				}
+			
+				@Override
+				public void init(SWTBot bot) {
+				}
+			
+				@Override
+				public String getFailureMessage() {
+					return null;
+				}
+			}, 15 * 60000); // 15 more minutes
+		}
+		this.bot.shell("Software Updates").bot().button("Not Now").click(); // Don't restart in test, test executor will do it.
+	}
+
+
+}
---
+++ tests/scripts/installation-updates/testInstall.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+usage() {
+	echo "Script to test installation"
+	echo "usage: $0 <eclipse_home> <file_containing_list_of_sites|repository_url>*"
+	echo "   <eclipse_home>: an eclipse installation will be performed on"
+	echo "   <file_containing_list_of_sites> a file containing a list of p2-friendly URLs of repositories"
+	echo "                                   separated by spaces or line breaks"
+	echo "   <repository_url>: URL of a p2 repo to install from"
+}
+
+# Takes repo URL as single parameter
+install_repo() {
+	echo "Installing content from " $1
+		report=TEST-install-$(date +%Y%m%d%H%M).xml
+			#Invoke tests
+		output=$(java \
+ -DUPDATE_SITE=$1 \
+ -Dorg.eclipse.swtbot.search.timeout=10000 \
+ -Dusage_reporting_enabled=false \
+ -Xms256M -Xmx768M -XX:MaxPermSize=512M \
+ -jar plugins/org.eclipse.equinox.launcher_*.jar \
+ -application org.eclipse.swtbot.eclipse.junit4.headless.swtbottestapplication \
+ -testApplication org.eclipse.ui.ide.workbench \
+ -product $productName \
+ -data workspace/ \
+ formatter=org.apache.tools.ant.taskdefs.optional.junit.XMLJUnitResultFormatter,$report \
+ formatter=org.apache.tools.ant.taskdefs.optional.junit.PlainJUnitResultFormatter \
+ -testPluginName org.jboss.tools.qa.install.tests \
+ -className org.jboss.tools.qa.install.tests.InstallTest \
+ -consoleLog -debug)
+		if [[ ! "$output" == *"Failures: 0, Errors: 0"* ]]; then
+			echo "Error while installing from " $site ". Read $report for details and see screenshots/"
+			popd
+			exit 1
+		fi
+}
+
+eclipse_home=$1
+shift
+
+if [ ! -d "$eclipse_home" -o ! -f "$eclipse_home/eclipse" ]; then
+	usage
+	exit 2
+fi
+if (( $# <= 0)); then
+	usage
+	exit 2
+fi
+
+echo "Preparing tests, installing framework"
+pushd $eclipse_home
+iniFile=$(ls -1 *.ini)
+productLineNumber=$(cat $iniFile | grep -n \\-product | cut -f 1 -d :)
+let "productLineNumber = $productLineNumber + 1"
+productName=$(sed -n ${productLineNumber}p $iniFile)
+
+#install test framework
+java -jar plugins/org.eclipse.equinox.launcher_*.jar \
+ -application org.eclipse.equinox.p2.director \
+ -repository http://download.eclipse.org/technology/swtbot/helios/dev-build/update-site/,http://download.jboss.org/jbosstools/builds/staging/jbosstools-3.3_trunk.component--tests/all/repo/ \
+ -installIU org.jboss.tools.tests.installation \
+ -consoleLog
+
+while (($#)); do
+	if [ -f "$1" ]; then
+		for repoUrl in $(cat "$1"); do
+			install_repo "$repoUrl"
+		done
+	else
+		install_repo "$1"
+	fi
+	shift
+done
+popd
+exit 0
