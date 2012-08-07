7c747ad4ea664e786c68246f347a9b8fd11b20dd
Jiri Peterka <peterka.jiri@gmail.com>
2012-08-07 13:37:43 +0000

    Dummy ui bot test multiple action added to check stability
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@42882 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/tests/org.jboss.tools.dummy.ui.bot.test/src/org/jboss/tools/dummy/ui/bot/test/DummyTest.java
+++ tests/tests/org.jboss.tools.dummy.ui.bot.test/src/org/jboss/tools/dummy/ui/bot/test/DummyTest.java
@@ -1,7 +1,8 @@
 package org.jboss.tools.dummy.ui.bot.test;
 
 
-import static org.junit.Assert.*;
+import static org.eclipse.swtbot.swt.finder.waits.Conditions.shellIsActive;
+import static org.junit.Assert.assertEquals;
 
 import java.io.OutputStreamWriter;
 
@@ -10,6 +11,8 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,8 +25,14 @@ import org.junit.runner.RunWith;
 @RunWith(SWTBotJunit4ClassRunner.class)
 public class DummyTest {
 	
+	static Logger log = Logger.getLogger(DummyTest.class);
+	
 	@BeforeClass
-	public static void before() {
+	public static void before() {		
+		ConsoleAppender ca = new ConsoleAppender();
+		ca.setWriter(new OutputStreamWriter(System.out));
+		ca.setLayout(new PatternLayout("[%-5p] %t: %m%n"));
+		log.addAppender(ca);
 	}
 
 	@Test
@@ -34,10 +43,24 @@ public class DummyTest {
 		ca.setLayout(new PatternLayout("[%-5p] %t: %m%n"));
 		log.addAppender(ca);
 
+		String pref = "Preferences";
 		SWTWorkbenchBot bot = new SWTWorkbenchBot();
-		bot.menu("Window").menu("Preferences").click();
-		assertEquals("Preferences",bot.activeShell().getText());
+		bot.menu("Window").menu(pref).click();
+		bot.waitUntil(shellIsActive(pref), 10000);
+		SWTBotShell shell = bot.shell(pref);
+		assertEquals(pref,shell.getText());
 		bot.activeShell().close();
+	}
+	
+	@Test
+	public void hundredTimes() {
+		for (int i = 0 ; i < 100; i++)
+			dummyTest();
+	}
+	
+	@AfterClass
+	public static void after() {
+		SWTWorkbenchBot bot = new SWTWorkbenchBot();
 		bot.closeAllShells();
 	}
 }
