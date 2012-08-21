5bde02c3351eeccacc60912e0f2883b8aca75fbc
Lucia Jelinkova <ljelinko@redhat.com>
2012-08-21 10:54:10 +0000

    Added activation of workbench shell (fix for windows)
    git-svn-id: http://svn.jboss.org/repos/jbosstools/branches/jbosstools-3.3.x@43128 a97e2381-89e5-4abb-bab3-167db6db766c

--- runtime/tests/org.jboss.tools.runtime.as.ui.bot.test/src/org/jboss/tools/runtime/as/ui/bot/test/dialog/preferences/PreferencesDialog.java
+++ runtime/tests/org.jboss.tools.runtime.as.ui.bot.test/src/org/jboss/tools/runtime/as/ui/bot/test/dialog/preferences/PreferencesDialog.java
@@ -1,6 +1,8 @@
 package org.jboss.tools.runtime.as.ui.bot.test.dialog.preferences;
 
+import org.apache.log4j.Logger;
 import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.jboss.tools.ui.bot.ext.SWTBotExt;
 import org.jboss.tools.ui.bot.ext.SWTBotFactory;
 import org.jboss.tools.ui.bot.ext.gen.ActionItem.Preference;
@@ -8,6 +10,8 @@ import org.jboss.tools.ui.bot.ext.gen.IPreference;
 
 public class PreferencesDialog {
 
+	private static final Logger log = Logger.getLogger(PreferencesDialog.class);
+	
 	protected void open(String... path){
 		SWTBotExt bot = SWTBotFactory.getBot();
 		try {
@@ -16,8 +20,23 @@ public class PreferencesDialog {
 		} catch (WidgetNotFoundException e){
 			// ok
 		}
+		activateWorkbenchShell();
 		IPreference preference = Preference.create(path);
 		SWTBotFactory.getOpen().preferenceOpen(preference);
+	}
+	
+	public void activateWorkbenchShell(){
+		log.info("Trying to activate workbench shell");
+		SWTBotShell[] shells = SWTBotFactory.getBot().shells();
+		if (shells.length == 1){
+			log.info("Only one shell present, assuming it's workbench and activating");
+			shells[0].activate();
+		} else {
+			log.info("More than one shell present");
+			for (SWTBotShell shell : shells){
+				log.info(shell.getText());
+			}
+		}
 	}
 	
 	public void ok(){
