fda224c3076bd06431c5c354df6ed1805f9a665e
Petr Suchy <psuchy@redhat.com>
2012-08-22 10:05:42 +0000

    git-svn-id: http://svn.jboss.org/repos/jbosstools/branches/jbosstools-3.3.x@43152 a97e2381-89e5-4abb-bab3-167db6db766c

--- runtime/tests/org.jboss.tools.runtime.as.ui.bot.test/src/org/jboss/tools/runtime/as/ui/bot/test/dialog/preferences/SeamPreferencesDialog.java
+++ runtime/tests/org.jboss.tools.runtime.as.ui.bot.test/src/org/jboss/tools/runtime/as/ui/bot/test/dialog/preferences/SeamPreferencesDialog.java
@@ -49,6 +49,7 @@ public class SeamPreferencesDialog extends PreferencesDialog {
 				e.printStackTrace();
 			}
 			robot.keyPress(KeyEvent.VK_RIGHT);
+			robot.keyRelease(KeyEvent.VK_ENTER);
 			robot.keyPress(KeyEvent.VK_ENTER);
 			robot.keyRelease(KeyEvent.VK_ENTER);
 			//KeyboardFactory.getAWTKeyboard().pressShortcut(Keystrokes.RIGHT, Keystrokes.CR, Keystrokes.LF);
