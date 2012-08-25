4a84334449342b45fa86283785d8a79274fbc526
Rob Stryker <rob.stryker@jboss.com>
2012-08-25 06:14:56 +0000

    JBIDE-12469 split of o.j.t.common and creation of o.j.t.common.core (missed messages files)
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@43233 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ common/plugins/org.jboss.tools.common.core/src/org/jboss/tools/common/core/Messages.java
@@ -0,0 +1,30 @@
+/******************************************************************************* 
+ * Copyright (c) 2009 - 2012 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/
+
+package org.jboss.tools.common.core;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "org.jboss.tools.common.core.messages"; //$NON-NLS-1$
+	public static String SAXValidator_IOExceptionMessage;
+	public static String SAXValidator_SAXExceptionMessage;
+	public static String SAXValidator_UnableToInstantiateMessage;
+	public static String XMLUtilities_IOExceptionMessage;
+	public static String XMLUtilities_SAXExceptionMessage;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
