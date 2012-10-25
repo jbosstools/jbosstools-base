278254f9ec8c0cb07f1da9027961f191ce91f7e8
Martin Malina <anyberry@gmail.com>
2012-10-25 13:52:01 +0200

    JBIDE-12943 Remove all swtbot tests that now live in jbosstools-integration-tests repo

--- tests/plugins/org.jboss.tools.ui.bot.ext/src/org/jboss/tools/ui/bot/ext/gen/IExport.java
+++
@@ -1,9 +0,0 @@
-/**
-* Generated Thu Apr 08 16:50:59 CEST 2010 JBDS 3.0.0-#50-GA
-*/
-package org.jboss.tools.ui.bot.ext.gen;
-
-import org.jboss.tools.ui.bot.ext.gen.IActionItem;
-
-public interface IExport extends IActionItem {
-}
