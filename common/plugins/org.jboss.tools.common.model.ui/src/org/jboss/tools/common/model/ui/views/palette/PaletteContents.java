e91da75b391307b7f2bc2a48107d8c8e2d9cc347
Viacheslav Kabanovich <scabanovich@exadel.com>
2013-02-28 13:45:55 -0800

    JBIDE-13679 HTML5 Palette should be available for any HTML5 file
    Mobile palette will be shown when <!DOCTYPE html> is present.

--- common/plugins/org.jboss.tools.common.model.ui/src/org/jboss/tools/common/model/ui/views/palette/PaletteContents.java
+++ common/plugins/org.jboss.tools.common.model.ui/src/org/jboss/tools/common/model/ui/views/palette/PaletteContents.java
@@ -14,13 +14,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IFileEditorInput;
-import org.jboss.tools.common.model.ui.ModelUIPlugin;
 import org.jboss.tools.common.model.ui.editor.EditorDescriptor;
+import org.jboss.tools.common.util.FileUtil;
 
 public class PaletteContents {
 	
@@ -50,24 +48,35 @@ public class PaletteContents {
 			return;
 		}
 		
-		IProject project = file.getProject();
+		
+		
 		List<String> natures = new ArrayList<String>();
-		try {
-			if (project.exists() && project.isOpen() && project.hasNature("org.jboss.tools.struts.strutsnature"))  //$NON-NLS-1$
-				natures.add("struts"); //$NON-NLS-1$
-		} catch (CoreException e) {
-			ModelUIPlugin.getPluginLog().logError(e);
+		String content = FileUtil.getContentFromEditorOrFile(file);
+		if(isDoctypeHTML(content)) {
+			natures.add("mobile"); //$NON-NLS-1$
+		} else {
+			natures.add("jsf"); //$NON-NLS-1$
 		}
-		try {
-			if (project.exists() && project.isOpen() && project.hasNature("org.jboss.tools.jsf.jsfnature"))  //$NON-NLS-1$
-				natures.add("jsf"); //$NON-NLS-1$
-		} catch (CoreException e) { 
-			ModelUIPlugin.getPluginLog().logError(e);
+//		IProject project = file.getProject();
+//		try {
+//			if (project.exists() && project.isOpen() && project.hasNature("org.jboss.tools.jsf.jsfnature"))  //$NON-NLS-1$
+//				natures.add("jsf"); //$NON-NLS-1$
+//		} catch (CoreException e) { 
+//			ModelUIPlugin.getPluginLog().logError(e);
+//		}
+		natureTypes = natures.toArray(new String[natures.size()]); 
+	}
+
+	boolean isDoctypeHTML(String content) {
+		int i = content.indexOf("<!DOCTYPE");
+		if(i >= 0) {
+			int j = content.indexOf(">", i);
+			if(j > i) {
+				String dt = content.substring(i + 9, j).trim();
+				return dt.equalsIgnoreCase("html");
+			}
 		}
-		if (natures.size() > 0) 
-			natureTypes = natures.toArray(new String[natures.size()]); 
-		else
-			natureTypes = new String[0];
+		return false;
 	}
 
 	private void emptyInit() {
