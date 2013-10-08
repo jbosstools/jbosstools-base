03a59557337d2c0540fdcec0c448bb4d1dd2b5a7
Viacheslav Kabanovich <scabanovich@exadel.com>
2013-10-08 11:36:04 -0700

    JBIDE-15625 Wrong warning for two renderers with the same renderer-type
    Renderer is unique by combination of family-type and renderer-type.

--- common/plugins/org.jboss.tools.common.model/src/org/jboss/tools/common/model/markers/ConstraintChecker.java
+++ common/plugins/org.jboss.tools.common.model/src/org/jboss/tools/common/model/markers/ConstraintChecker.java
@@ -43,17 +43,29 @@ public class ConstraintChecker {
 	private void check(XModelObject o) {
 		XAttribute[] as = o.getModelEntity().getAttributes();
 		String idAttr = null;
+		String idAttr2 = null;
 		for (int i = 0; i < as.length; i++) {
 			String error = as[i].getConstraint().getError(o.getAttributeValue(as[i].getName()));
 			if(error != null) addProblem(o, as[i].getName(), "Value " + error);
-			if("true".equals(as[i].getProperty("id"))) idAttr = as[i].getName();
+			if("true".equals(as[i].getProperty("id"))) {
+				if(idAttr == null) {
+					idAttr = as[i].getName();
+				} else {
+					idAttr2 = as[i].getName();
+				}
+			}
 		}
 		XModelObject[] cs = ((XModelObjectImpl)o).getLoadedChildren();
 		for (int i = 0; i < cs.length; i++) check(cs[i]);
 
 		String duplicate = o.get(XModelObjectImpl.DUPLICATE);
 		if(duplicate != null && duplicate.length() > 0 && idAttr != null) {
-			addProblem(o, idAttr, "Value " + o.getAttributeValue(idAttr) + " is not unique.");
+			String message = "Value " + o.getAttributeValue(idAttr) + " is not unique.";
+			if(idAttr2 != null) {
+				message = "Combination of " + idAttr + "=" + o.getAttributeValue(idAttr)
+						+ ", " + idAttr2 + "=" + o.getAttributeValue(idAttr2) + " is not unique.";
+			}
+			addProblem(o, idAttr, message);
 		}
 	}
 
