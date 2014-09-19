7ac696c5069d431c57c08bfa18187a5a01ed9926
Alexey Kazakov <akazakov@exadel.com>
2014-09-19 12:12:36 -0700

    JBDS-3159 Usage dialog is not displayed for user after initial install

--- usage/plugins/org.jboss.tools.usage/src/org/jboss/tools/usage/internal/preferences/GlobalUsageSettings.java
+++ usage/plugins/org.jboss.tools.usage/src/org/jboss/tools/usage/internal/preferences/GlobalUsageSettings.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 Red Hat, Inc.
+ * Copyright (c) 2010-2014 Red Hat, Inc.
  * Distributed under license by Red Hat, Inc. All rights reserved.
  * This program is made available under the terms of the
  * Eclipse Public License v1.0 which accompanies this distribution,
@@ -85,6 +85,7 @@ public class GlobalUsageSettings {
 
 			return Boolean.valueOf(isEnabled.toString());
 		} catch (Exception e) {
+			JBossToolsUsageActivator.getDefault().getLogger().error(e, false);
 			return ALLINSTANCES_USAGE_REPORTING_ENABLED_DEFAULT;
 		}
 	}
