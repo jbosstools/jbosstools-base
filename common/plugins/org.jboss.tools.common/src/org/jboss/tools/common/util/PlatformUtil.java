47306a722eda576718e8a40d0cb675fe73040c09
Ilya Buziuk <ibuziuk@exadel.com>
2015-09-03 14:21:31 +0300

    JBIDE-20524: Adding useful API for OS detection

--- common/plugins/org.jboss.tools.common/src/org/jboss/tools/common/util/PlatformUtil.java
+++ common/plugins/org.jboss.tools.common/src/org/jboss/tools/common/util/PlatformUtil.java
@@ -15,9 +15,45 @@ import java.lang.reflect.Field;
 import org.eclipse.core.runtime.Platform;
 
 public class PlatformUtil {
-
 	private static final String SWT_GTK3 = "SWT_GTK3"; //$NON-NLS-1$
+	private static final String MAC = "mac"; //$NON-NLS-1$
+	private static final String DARWIN = "darwin"; //$NON-NLS-1$
+	private static final String WIN = "win"; //$NON-NLS-1$
+	private static final String LINUX = "nux"; //$NON-NLS-1$
+	private static OS detectedOs;
 	
+	public static boolean isWindows() {
+		return OS.WINDOWS.equals(getOs());
+	}
+
+	public static boolean isMacOS() {
+		return OS.MACOS.equals(getOs());
+	}
+
+	public static boolean isLinux() {
+		return OS.LINUX.equals(getOs());
+	}
+
+	public static OS getOs() {
+		if (detectedOs == null) {
+			String currentOs = System.getProperty("os.name", "generic").toLowerCase(); //$NON-NLS-1$ //$NON-NLS-2$
+			if ((currentOs.indexOf(MAC) >= 0) || (currentOs.indexOf(DARWIN) >= 0)) {
+				detectedOs = OS.MACOS;
+			} else if (currentOs.indexOf(WIN) >= 0) {
+				detectedOs = OS.WINDOWS;
+			} else if (currentOs.indexOf(LINUX) >= 0) {
+				detectedOs = OS.LINUX;
+			} else {
+				detectedOs = OS.OTHER;
+			}
+		}
+		return detectedOs;
+	}
+
+	public enum OS {
+		WINDOWS, MACOS, LINUX, OTHER
+	}
+		
 	public static boolean isGTK3() {
 		if (Platform.WS_GTK.equals(Platform.getWS())) {
 			try {
@@ -47,5 +83,4 @@ public class PlatformUtil {
 		}
 		return !"0".equals(gtk3); //$NON-NLS-1$
 	}
-	
 }
