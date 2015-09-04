1eb4d392210ac620a06f869caaaa184c7f66b7e3
Denis Golovin <dgolovin@gmail.com>
2015-09-04 15:28:30 -0700

    Add copyright comment where it is missing

--- common/tests/org.jboss.tools.common.model.test/src/org/jboss/tools/common/model/test/PaletteLoaderTest.java
+++ common/tests/org.jboss.tools.common.model.test/src/org/jboss/tools/common/model/test/PaletteLoaderTest.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+  * Copyright (c) 2010 - 2015 Red Hat, Inc.
+  * Distributed under license by Red Hat, Inc. All rights reserved.
+  * This program is made available under the terms of the
+  * Eclipse Public License v1.0 which accompanies this distribution,
+  * and is available at http://www.eclipse.org/legal/epl-v10.html
+  *
+  * Contributor:
+  *     Red Hat, Inc. - initial API and implementation
+  ******************************************************************************/
 package org.jboss.tools.common.model.test;
 
 import java.util.HashSet;
--- runtime/tests/org.jboss.tools.runtime.test/src/org/jboss/tools/runtime/test/RuntimePathUtilTest.java
+++ runtime/tests/org.jboss.tools.runtime.test/src/org/jboss/tools/runtime/test/RuntimePathUtilTest.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+  * Copyright (c) 2010 - 2015 Red Hat, Inc.
+  * Distributed under license by Red Hat, Inc. All rights reserved.
+  * This program is made available under the terms of the
+  * Eclipse Public License v1.0 which accompanies this distribution,
+  * and is available at http://www.eclipse.org/legal/epl-v10.html
+  *
+  * Contributor:
+  *     Red Hat, Inc. - initial API and implementation
+  ******************************************************************************/
 package org.jboss.tools.runtime.test;
 
 import static org.jboss.tools.runtime.core.util.RuntimeModelUtil.IN_LINE_DELIMITER;
