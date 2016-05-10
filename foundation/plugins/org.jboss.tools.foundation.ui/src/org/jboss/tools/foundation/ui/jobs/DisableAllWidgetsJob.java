b695657c1009896f33e0cec71232a425ed0db736
Rob Stryker <rob@oxbeef.net>
2016-05-10 19:17:45 -0400

    JBIDE-22325 - Move job to disable all widgets into foundation, and allow AStools to use it (#503)

---
+++ foundation/plugins/org.jboss.tools.foundation.ui/src/org/jboss/tools/foundation/ui/jobs/DisableAllWidgetsJob.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+  * Copyright (c) 2016 Red Hat, Inc.
+  * Distributed under license by Red Hat, Inc. All rights reserved.
+  * This program is made available under the terms of the
+  * Eclipse Public License v1.0 which accompanies this distribution,
+  * and is available at http://www.eclipse.org/legal/epl-v10.html
+  *
+  * Contributor:
+  *     Red Hat, Inc. - initial API and implementation
+  ******************************************************************************/
+package org.jboss.tools.foundation.ui.jobs;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.swt.graphics.Cursor;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.progress.UIJob;
+import org.jboss.tools.foundation.ui.widget.WidgetUtility;
+
+public class DisableAllWidgetsJob extends UIJob {
+
+	private boolean disableWidgets;
+	private Cursor cursor;
+	private boolean disableCursor;
+	private Composite container;
+
+	public DisableAllWidgetsJob(boolean disable, Composite container, Cursor cursor) {
+		this(disable, container, disable, cursor);
+	}
+	
+	public DisableAllWidgetsJob(boolean disableWidgets, Composite container, boolean disableCursor, Cursor cursor) {
+		super((disableWidgets? "Disabling" : "Enabling") + " all controls...");
+		this.disableWidgets = disableWidgets;
+		this.container = container;
+		this.disableCursor = disableCursor;
+		this.cursor = cursor;
+	}
+
+	@Override
+	public IStatus runInUIThread(IProgressMonitor monitor) {
+		run();
+		return Status.OK_STATUS;
+	}
+	
+	/*
+	 * Only to be called from UI thread
+	 */
+	public void run() {
+		new WidgetUtility().enableAllChildren(!disableWidgets, container);
+		if (!disableCursor) {
+			container.setCursor(null);
+			if (cursor != null) {
+				cursor.dispose();
+			}
+		} else {
+			if (cursor != null) {
+				container.setCursor(cursor);
+			}
+		}
+	}
+}
