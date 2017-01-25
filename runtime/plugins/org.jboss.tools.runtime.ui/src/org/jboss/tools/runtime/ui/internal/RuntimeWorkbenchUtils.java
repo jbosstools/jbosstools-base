f264aa7b4bfb9b987a36c4355b12e872f1bc899d
Rob Stryker <rob@oxbeef.net>
2017-01-25 11:54:45 -0500

    JBDS-4228 - check not just if platform is running, but that it is not closing

--- runtime/plugins/org.jboss.tools.runtime.ui/src/org/jboss/tools/runtime/ui/internal/RuntimeWorkbenchUtils.java
+++ runtime/plugins/org.jboss.tools.runtime.ui/src/org/jboss/tools/runtime/ui/internal/RuntimeWorkbenchUtils.java
@@ -16,18 +16,19 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.dialogs.PreferencesUtil;
+import org.eclipse.ui.internal.Workbench;
 import org.eclipse.ui.navigator.CommonNavigator;
 import org.jboss.tools.runtime.ui.internal.preferences.RuntimePreferencePage;
 
 public class RuntimeWorkbenchUtils {
 	public static void refreshServersView() {
 		// https://jira.jboss.org/jira/browse/JBDS-1091
-		if (!PlatformUI.isWorkbenchRunning()) {
+		if (!PlatformUI.isWorkbenchRunning() ||  PlatformUI.getWorkbench().isClosing()) {
 			return;
 		}
 		Display.getDefault().asyncExec(new Runnable() {
 			public void run() {
-				if (PlatformUI.isWorkbenchRunning()) {
+				if (PlatformUI.isWorkbenchRunning()  && !PlatformUI.getWorkbench().isClosing()) {
 					IViewPart view = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().findView("org.eclipse.wst.server.ui.ServersView"); //$NONE-NLS-1$ //$NON-NLS-1$
 					if (view instanceof CommonNavigator) {
 						CommonNavigator navigator = (CommonNavigator) view;
