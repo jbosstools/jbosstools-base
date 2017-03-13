ecea892b06681f0583df3a9e5d9827ca2fada10e
nickboldt <nboldt@redhat.com>
2017-03-13 09:20:33 -0400

    JBIDE-23833 more complete solution to remove JIRA support from org.jboss.tools.common.mylyn, including removing test in common/tests/org.jboss.tools.common.mylyn.test/src/org/jboss/tools/common/mylyn/test/RepositoryTest.java and bumping all containing features too
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- common/tests/org.jboss.tools.common.mylyn.test/src/org/jboss/tools/common/mylyn/test/RepositoryTest.java
+++ common/tests/org.jboss.tools.common.mylyn.test/src/org/jboss/tools/common/mylyn/test/RepositoryTest.java
@@ -1,5 +1,5 @@
 /******************************************************************************* 
- * Copyright (c) 2011 Red Hat, Inc. 
+ * Copyright (c) 2011-2017 Red Hat, Inc. 
  * Distributed under license by Red Hat, Inc. All rights reserved. 
  * This program is made available under the terms of the 
  * Eclipse Public License v1.0 which accompanies this distribution, 
@@ -26,8 +26,9 @@ public class RepositoryTest extends TestCase {
 	}
 
 	// FIXME Enable this test when JIRA connector plugins are added to TP & Update site
-	public void _testJIRA() {
-		TaskRepository repo = TasksUiPlugin.getRepositoryManager().getRepository("jira", "https://issues.jboss.org");
-		assertNotNull(repo);
-	}
+	// DISABLED as part of JBIDE-23833
+	// public void _testJIRA() {
+	// 	TaskRepository repo = TasksUiPlugin.getRepositoryManager().getRepository("jira", "https://issues.jboss.org");
+	// 	assertNotNull(repo);
+	// }
 }
