26c05d2bdcd32a35c4026fff311528d6eb05f68e
Jeff MAURY <jmaury@redhat.com>
2017-04-05 09:21:31 +0200

    Add trace message

--- tests/plugins/org.jboss.tools.tests/src/org/jboss/tools/test/util/JobUtils.java
+++ tests/plugins/org.jboss.tools.tests/src/org/jboss/tools/test/util/JobUtils.java
@@ -58,6 +58,7 @@ public class JobUtils {
 			Job[] jobs = Job.getJobManager().find(null);
 			for (Job job : jobs) {
 			    if (job.isSystem() || job.getState() == Job.SLEEPING) {
+			        System.out.println("Skipped job " + job.getName() + "isSystem=" + job.isSystem() + " state=" + job.getState());
 			        continue;
 			    }
 				if (!shouldIgnoreJob(job)) {
