3ba275ba6c2c49ee372e50eeddf93657353a3206
nickboldt <nboldt@redhat.com>
2017-04-30 10:53:42 -0400

    JBIDE-24378 Option 1: fix ResolverProjectNature2 so it implements ELResolver
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- common/tests/org.jboss.tools.common.el.core.test/src/org/jboss/tools/common/el/core/test/resolver/ResolverProjectNature2.java
+++ common/tests/org.jboss.tools.common.el.core.test/src/org/jboss/tools/common/el/core/test/resolver/ResolverProjectNature2.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-  * Copyright (c) 2010 - 2012 Red Hat, Inc.
+  * Copyright (c) 2010 - 2017 Red Hat, Inc.
   * Distributed under license by Red Hat, Inc. All rights reserved.
   * This program is made available under the terms of the
   * Eclipse Public License v1.0 which accompanies this distribution,
@@ -9,10 +9,49 @@
   *     Red Hat, Inc. - initial API and implementation
   ******************************************************************************/
 package org.jboss.tools.common.el.core.test.resolver;
+import java.util.List;
 
-public class ResolverProjectNature2 {
+import org.eclipse.jdt.core.IJavaElement;
+import org.jboss.tools.common.el.core.model.ELExpression;
+import org.jboss.tools.common.el.core.parser.ELParserFactory;
+import org.jboss.tools.common.el.core.resolver.ELContext;
+import org.jboss.tools.common.el.core.resolver.ELResolution;
+import org.jboss.tools.common.el.core.resolver.ELResolver;
+import org.jboss.tools.common.el.core.resolver.IRelevanceCheck;
+import org.jboss.tools.common.text.TextProposal;
+
+
+public class ResolverProjectNature2 implements ELResolver {
 
 	public ResolverProjectNature2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<TextProposal> getProposals(ELContext context, String el,
+			int offset) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<TextProposal> getProposals(ELContext context, int offset) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ELResolution resolve(ELContext context, ELExpression operand,
+			int offset) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ELParserFactory getParserFactory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public IRelevanceCheck createRelevanceCheck(IJavaElement element) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
