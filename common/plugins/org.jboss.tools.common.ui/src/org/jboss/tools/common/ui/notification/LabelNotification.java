5b40c507f0bd4cd7a05eb8a1715af91bf7a1fa45
Jeff MAURY <jmaury@redhat.com>
2020-04-10 08:10:54 +0200

    JBIDE-27194: Missing artifact io.quarkus:quarkus-smallrye-metrics:jar:${quarkus.version}
    - Added new notification helper
    
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

---
+++ common/plugins/org.jboss.tools.common.ui/src/org/jboss/tools/common/ui/notification/LabelNotification.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.common.ui.notification;
+
+import org.eclipse.mylyn.commons.ui.dialogs.AbstractNotificationPopup;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+
+/**
+ * @author Red Hat Developers
+ *
+ */
+public class LabelNotification {
+	private static class WidgetNotification extends AbstractNotificationPopup {
+
+		private String text;
+
+		public WidgetNotification(Shell shell, String text) {
+			super(shell.getDisplay());
+			this.text = text;
+		}
+
+		@Override
+		protected void createContentArea(Composite parent) {
+			Label label = new Label(parent, SWT.NONE);
+			label.setText(text);
+		}
+	}
+
+	private WidgetNotification window;
+
+	public void close() {
+		if (window != null) {
+			window.close();
+		}
+	}
+
+	public static LabelNotification openNotification(LabelNotification previous, Shell shell, String text) {
+		LabelNotification notification = new LabelNotification();
+
+		shell.getDisplay().asyncExec(() -> {
+			if (previous != null) {
+				previous.close();
+			}
+			notification.window = new WidgetNotification(shell, text);
+			notification.window.open();
+		});
+		return notification;
+	}
+
+	public static LabelNotification openNotification(Shell shell, String text) {
+		return openNotification(null, shell, text);
+	}
+}
