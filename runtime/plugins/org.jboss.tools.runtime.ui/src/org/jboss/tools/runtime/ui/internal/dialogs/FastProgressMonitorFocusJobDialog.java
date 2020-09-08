8d6282804b2b3e1722af6e8b572b373c7ab9b54a
Stephane Bouchet <sbouchet@redhat.com>
2020-09-08 10:28:18 +0200

    refactor to fix code smells
    Signed-off-by: Stephane Bouchet <sbouchet@redhat.com>

--- runtime/plugins/org.jboss.tools.runtime.ui/src/org/jboss/tools/runtime/ui/internal/dialogs/FastProgressMonitorFocusJobDialog.java
+++ runtime/plugins/org.jboss.tools.runtime.ui/src/org/jboss/tools/runtime/ui/internal/dialogs/FastProgressMonitorFocusJobDialog.java
@@ -14,40 +14,14 @@ package org.jboss.tools.runtime.ui.internal.dialogs;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IProgressMonitorWithBlocking;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.IJobChangeEvent;
-import org.eclipse.core.runtime.jobs.IJobChangeListener;
 import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.core.runtime.jobs.JobChangeAdapter;
-import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.operation.IRunnableWithProgress;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.BusyIndicator;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.TraverseEvent;
-import org.eclipse.swt.events.TraverseListener;
-import org.eclipse.swt.graphics.Rectangle;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.internal.IPreferenceConstants;
-import org.eclipse.ui.internal.WorkbenchMessages;
-import org.eclipse.ui.internal.WorkbenchPlugin;
 import org.eclipse.ui.internal.progress.ProgressManager;
 import org.eclipse.ui.internal.progress.ProgressManager.JobMonitor;
 import org.eclipse.ui.internal.progress.ProgressMonitorFocusJobDialog;
-import org.eclipse.ui.progress.IProgressConstants;
-import org.eclipse.ui.progress.WorkbenchJob;
 import org.jboss.tools.runtime.ui.RuntimeUIActivator;
 
 /**
@@ -101,54 +75,14 @@ public class FastProgressMonitorFocusJobDialog extends ProgressMonitorFocusJobDi
 	private IRunnableWithProgress runnableWithProgress(final Job longJob) {
 		IRunnableWithProgress runnable = new IRunnableWithProgress(){
 			public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
-				addRunableMonitorAsListener(longJob, getBlockingProgressMonitor(monitor));
+				addRunableMonitorAsListener(longJob, monitor);
 				longJob.join();
 			}
 		};
 		return runnable;
 	}
 	
-	private IProgressMonitorWithBlocking getBlockingProgressMonitor(final IProgressMonitor monitor) {
-		IProgressMonitorWithBlocking listener = new IProgressMonitorWithBlocking(){
-			public void beginTask(String name, int totalWork) {
-				monitor.beginTask(name, totalWork);
-			}
-			public void done() {
-				monitor.done();
-			}
-			public void internalWorked(double work) {
-				monitor.internalWorked(work);
-			}
-			public boolean isCanceled() {
-				return monitor.isCanceled();
-			}
-			public void setCanceled(boolean value) {
-				monitor.setCanceled(value);
-			}
-			public void setTaskName(String name) {
-				monitor.setTaskName(name);
-			}
-			public void subTask(String name) {
-				monitor.subTask(name);
-			}
-			public void worked(int work) {
-				monitor.worked(work);
-			}
-			public void setBlocked(IStatus reason) {
-				if( monitor instanceof IProgressMonitorWithBlocking) {
-					((IProgressMonitorWithBlocking)monitor).setBlocked(reason);
-				}
-			}
-			public void clearBlocked() {
-				if( monitor instanceof IProgressMonitorWithBlocking) {
-					((IProgressMonitorWithBlocking)monitor).clearBlocked();
-				}
-			}
-		};
-		return listener;
-	}
-	
-	private void addRunableMonitorAsListener(Job longJob, IProgressMonitorWithBlocking listener) {
+	private void addRunableMonitorAsListener(Job longJob, IProgressMonitor listener) {
 		JobMonitor jobMonitor = ProgressManager.getInstance().progressFor(longJob);
 		try {
 			jobMonitor.addProgressListener(listener);
