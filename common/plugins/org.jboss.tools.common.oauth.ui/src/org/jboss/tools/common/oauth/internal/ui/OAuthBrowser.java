bfe5b9f0f53feea4992719362c15c30d9d6fff30
Andre Dietisheim <adietish@redhat.com>
2021-06-22 14:59:14 +0200

    JBIDE-27895: allow macos/safari to redirect to non-https urls (#700)
    * JBIDE-27895: allow macos/safari to redirect to non-https urls
    
    Signed-off-by: Andre Dietisheim <adietish@redhat.com>

--- common/plugins/org.jboss.tools.common.oauth.ui/src/org/jboss/tools/common/oauth/internal/ui/OAuthBrowser.java
+++ common/plugins/org.jboss.tools.common.oauth.ui/src/org/jboss/tools/common/oauth/internal/ui/OAuthBrowser.java
@@ -11,11 +11,13 @@
 package org.jboss.tools.common.oauth.internal.ui;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.net.URISyntaxException;
 import java.util.concurrent.CompletableFuture;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.swt.SWT;
@@ -24,7 +26,6 @@ import org.eclipse.swt.events.DisposeEvent;
 import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.jboss.tools.common.oauth.core.LoginResponse;
 import org.keycloak.OAuthErrorException;
@@ -51,8 +52,52 @@ public class OAuthBrowser extends Composite implements DisposeListener {
     addDisposeListener(this);
     setLayout(new GridLayout(1, false));
     setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, true));
+    allowNonHttpsRedirects();
     browser = new Browser(this, SWT.NONE);
     browser.setLayoutData(new GridData( GridData.FILL_BOTH));
+  }
+
+  /**
+   * Set the transport policy on MacOS to allow redirects to non-https urls.
+   * 
+   * @see <a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=568749">Eclipse bug 568749</a>
+   */
+  private void allowNonHttpsRedirects() {
+	if (!Platform.OS_MACOSX.equals(Platform.getOS())) {
+		return;
+	}
+
+	try {
+		/*
+		 * 	NSDictionary allowNonHttps = NSDictionary.dictionaryWithObject(
+		 *	            NSNumber.numberWithBool(true),
+		 *	            NSString.stringWith("NSAllowsArbitraryLoads"));
+		 */
+		Class<?> nsNumberClass = Class.forName("org.eclipse.swt.internal.cocoa.NSNumber");
+		Method numberWithBoolMethod = nsNumberClass.getDeclaredMethod("numberWithBool", Boolean.TYPE);
+		Class<?> nsStringClass = Class.forName("org.eclipse.swt.internal.cocoa.NSString");
+		Method stringWithMethod = nsStringClass.getDeclaredMethod("stringWith", String.class);
+		Class<?> nsDictionnaryClass = Class.forName("org.eclipse.swt.internal.cocoa.NSDictionary");
+		Class<?> idClass = Class.forName("org.eclipse.swt.internal.cocoa.id");
+		Method dictionnaryWithObjectMethod = nsDictionnaryClass.getMethod("dictionaryWithObject", idClass, idClass);
+		Object allowNonHttps = dictionnaryWithObjectMethod.invoke(dictionnaryWithObjectMethod,
+				numberWithBoolMethod.invoke(nsNumberClass, true),
+				stringWithMethod.invoke(nsStringClass, "NSAllowsArbitraryLoads"));
+
+		/*
+		 * 	    NSBundle.mainBundle().infoDictionary().setValue(
+	     *       	allowNonHttps, NSString.stringWith("NSAppTransportSecurity"));
+		 */
+		Class<?> nsBundle = Class.forName("org.eclipse.swt.internal.cocoa.NSBundle");
+		Object mainBundle = nsBundle.getDeclaredMethod("mainBundle").invoke(nsBundle);
+		Object infoDictionary = mainBundle.getClass().getDeclaredMethod("infoDictionary").invoke(mainBundle);
+		Method setValue = infoDictionary.getClass().getMethod("setValue", idClass, nsStringClass);
+		setValue.invoke(infoDictionary,
+				allowNonHttps,
+				stringWithMethod.invoke(stringWithMethod, "NSAppTransportSecurity"));
+	} catch (ReflectiveOperationException | SecurityException | IllegalArgumentException e) {
+		CommonOAuthUIActivator.logError("Could not allow non-https redirects.", e);
+	}
   }
   
   private IStatus processRedirect(IProgressMonitor monitor) {
